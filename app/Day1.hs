226692945da284a4160438efa17b6a9380526d6e
JMaximusIX <nix@gibts>
2023-12-01 09:43:01 +0100

    better version

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,27 +1,24 @@
 module Day1 (part1, part2) where
 
+import Advent (Part (Part1, Part2))
 import Data.Char (digitToInt, isDigit)
-import Data.List (dropWhileEnd, elemIndex, find, isPrefixOf, isSuffixOf)
+import Data.List (elemIndex, find, isPrefixOf, isSuffixOf)
 import Data.Maybe (fromMaybe)
 
 part1, part2 :: String -> Int
-part1 input = sum $ map calibrationValue1 (lines input)
-part2 input = sum $ map calibrationValue2 (lines input)
+part1 input = sum $ map (calibrationValue Part1) (lines input)
+part2 input = sum $ map (calibrationValue Part2) (lines input)
 
-calibrationValue1 :: String -> Int
-calibrationValue1 line = read (firstDigit : [lastDigit])
-  where
-    firstDigit = head $ dropWhile (not . isDigit) line
-    lastDigit = last $ dropWhileEnd (not . isDigit) line
-
-calibrationValue2 :: String -> Int
-calibrationValue2 line = 10 * firstDigit line + lastDigit line
+calibrationValue :: Part -> String -> Int
+calibrationValue part line = 10 * firstDigit line + lastDigit line
   where
     digitWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     maybe_digit condition = maybe 0 (+ 1) . (`elemIndex` digitWords) <$> find condition digitWords
     firstDigit l@(x : xs)
       | isDigit x = digitToInt x
+      | part == Part1 = firstDigit xs
       | otherwise = fromMaybe (firstDigit xs) (maybe_digit (`isPrefixOf` l))
     lastDigit l
       | isDigit (last l) = digitToInt (last l)
+      | part == Part1 = lastDigit (init l)
       | otherwise = fromMaybe (lastDigit (init l)) (maybe_digit (`isSuffixOf` l))
