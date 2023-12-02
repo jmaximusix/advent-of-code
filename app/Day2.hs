e05f6c8a4f8647a7adb2d17f52c0c71329c485e2
JMaximusIX <nix@gibts>
2023-12-02 07:57:35 +0100

    v2

--- app/Day2.hs
+++ app/Day2.hs
@@ -1,19 +1,18 @@
 module Day2 (part1, part2) where
 
-import Data.Char (toUpper)
-import Data.List (uncons)
+import Data.Bifunctor (bimap)
 import Data.List.Split (chunksOf, splitOn)
 import qualified Data.Map as Map (Map, empty, fromList, insertWith, toList, (!))
-import Data.Maybe (fromJust)
+import GHC.Utils.Misc (capitalise)
 
 data Color = Red | Green | Blue deriving (Show, Eq, Ord, Read)
 
 part1, part2 :: String -> Int
-part1 input = sum $ map fst $ filter (compareCubes (Map.fromList [(Red, 12), (Green, 13), (Blue, 14)]) . snd) $ map parseInput (lines input)
+part1 input = sum $ map fst $ filter (hasMoreCubes (Map.fromList [(Red, 12), (Green, 13), (Blue, 14)]) . snd) $ map parseInput (lines input)
 part2 input = sum $ map (setPower . snd . parseInput) (lines input)
 
-compareCubes :: Map.Map Color Int -> Map.Map Color Int -> Bool
-compareCubes a b = all (\color -> b Map.! color <= a Map.! color) [Red, Green, Blue]
+hasMoreCubes :: Map.Map Color Int -> Map.Map Color Int -> Bool
+hasMoreCubes a b = all (\color -> a Map.! color >= b Map.! color) [Red, Green, Blue]
 
 setPower :: Map.Map Color Int -> Int
 setPower cubes = product $ map snd $ Map.toList cubes
@@ -21,7 +20,7 @@ setPower cubes = product $ map snd $ Map.toList cubes
 parseInput :: String -> (Int, Map.Map Color Int)
 parseInput input = (game_id, foldl update Map.empty cubes)
   where
-    update m [a, b : bs] = Map.insertWith max (read (toUpper b : init bs)) (read a) m
-    pairs = chunksOf 2 $ splitOn " " (input ++ ",")
-    (game, cubes) = fromJust $ uncons pairs
-    game_id = read $ init $ last game
+    update m (cube, n) = Map.insertWith max cube n m
+    pairs = map (\[a, b] -> (a, init b)) $ chunksOf 2 $ splitOn " " (input ++ ",")
+    cubes = map (bimap read (read . capitalise)) (tail pairs)
+    game_id = (read . snd . head) pairs
