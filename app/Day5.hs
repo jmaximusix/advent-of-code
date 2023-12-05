4d225323b357c37c63c4dcc7f0ef2d3b998f30a7
JMaximusIX <nix@gibts>
2023-12-05 09:01:34 +0100

    proper namings

--- app/Day5.hs
+++ app/Day5.hs
@@ -3,39 +3,39 @@ module Day5 (part1, part2) where
 import Data.List.Extra (chunksOf)
 import Data.List.Split (splitOn)
 
+type Range = (Int, Int)
+
 part1, part2 :: [String] -> Int
-part1 input = minimum $ foldl stuffMap (seeds1 input) $ parseInput input
-part2 input = minimum $ map fst $ foldl stuffMap2 (seeds2 input) $ parseInput input
-
-seeds1 :: [String] -> [Int]
-seeds1 = map read . tail . words . head
-
-seeds2 :: [String] -> [(Int, Int)]
-seeds2 input = map (\[a, b] -> (a, b)) $ chunksOf 2 $ seeds1 input
-
-stuffMap :: [Int] -> [[Int]] -> [Int]
-stuffMap seeds lookupgedöns = map (lookupShit lookupgedöns) seeds
-
-lookupShit :: [[Int]] -> Int -> Int
-lookupShit [] seed = seed
-lookupShit ([dest, source, len] : xs) seed
-  | seed >= source && seed < source + len = dest + seed - source
-  | otherwise = lookupShit xs seed
+part1 input = minimum $ foldl applyOneMap (seeds1 input) $ parseInput input
+part2 input = minimum $ map fst $ foldl applyOneMap2 (seeds2 input) $ parseInput input
 
 parseInput :: [String] -> [[[Int]]]
 parseInput input = map (map (map (\x -> read x :: Int) . words) . tail . lines) $ splitOn "\n\n" (unlines $ tail input)
 
-stuffMap2 :: [(Int, Int)] -> [[Int]] -> [(Int, Int)]
-stuffMap2 seeds lookupgedöns = concatMap (lookupShit2 lookupgedöns) seeds
+seeds1 :: [String] -> [Int]
+seeds1 = map read . tail . words . head
 
-lookupShit2 :: [[Int]] -> (Int, Int) -> [(Int, Int)]
-lookupShit2 [] range = [range]
-lookupShit2 ([dest, source, len] : xs) range = inside' ++ concatMap (lookupShit2 xs) outside
+seeds2 :: [String] -> [Range]
+seeds2 input = map (\[a, b] -> (a, b)) $ chunksOf 2 $ seeds1 input
+
+applyOneMap :: [Int] -> [[Int]] -> [Int]
+applyOneMap seeds rangeMap = map (applyToSeed rangeMap) seeds
+
+applyToSeed :: [[Int]] -> Int -> Int
+applyToSeed [] seed = seed
+applyToSeed ([dest, source, len] : xs) seed
+  | seed >= source && seed < source + len = dest + seed - source
+  | otherwise = applyToSeed xs seed
+
+applyOneMap2 :: [Range] -> [[Int]] -> [Range]
+applyOneMap2 seeds rangeMap = concatMap (applyToSeed2 rangeMap) seeds
+
+applyToSeed2 :: [[Int]] -> Range -> [Range]
+applyToSeed2 [] range = [range]
+applyToSeed2 ([dest, source, len] : xs) range = inside' ++ concatMap (applyToSeed2 xs) outside
   where
     (inside, outside) = range `intersect` (source, len)
     inside' = map (\(a, b) -> (dest + a - source, b)) inside
-
-type Range = (Int, Int)
 
 -- a is the range that gets split on b, seperated in inside and outside
 intersect :: Range -> Range -> ([Range], [Range])
