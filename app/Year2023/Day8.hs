5090f8c77db64a61929fc6bf1f891e99e921187d
JMaximusIX <nix@gibts>
2023-12-08 07:06:35 +0100

    Day 8

--- app/Year2023/Day8.hs
+++ app/Year2023/Day8.hs
@@ -1,5 +1,37 @@
 module Year2023.Day8 (part1, part2) where
 
+import Data.Bifunctor (bimap)
+import Data.List (findIndex, uncons)
+import Data.List.Extra (splitOn)
+import qualified Data.Map as Map (Map, fromList, keys, (!))
+import Data.Maybe (fromJust)
+
+data Instruction = L | R deriving (Show, Eq, Read)
+
+type Nodes = Map.Map String (String, String)
+
 part1, part2 :: [String] -> Int
-part1 = undefined
-part2 = undefined
+part1 input = findLen (== "ZZZ") (parseInput input) "AAA"
+part2 input = foldl1 lcm $ map (findLen ((== 'Z') . last) parsed) endingInA
+  where
+    parsed@(_, nodes) = parseInput input
+    endingInA = filter ((== 'A') . last) $ Map.keys nodes
+
+findLen :: (String -> Bool) -> ([Instruction], Nodes) -> String -> Int
+findLen stopc (insts, nodes) start = fromJust $ findIndex stopc $ scanl (doStep nodes) start insts
+
+parseInput :: [String] -> ([Instruction], Nodes)
+parseInput = bimap (cycle . map (read . (: []))) (Map.fromList . map parseNode . tail) . fromJust . uncons
+
+parseNode :: String -> (String, (String, String))
+parseNode node = (name, (left, right))
+  where
+    [name, adjacent] = splitOn " = " node
+    [left, right] = (splitOn ", " . init . tail) adjacent
+
+doStep :: Nodes -> String -> Instruction -> String
+doStep nodes current inst = case inst of
+  L -> left
+  R -> right
+  where
+    (left, right) = nodes Map.! current
