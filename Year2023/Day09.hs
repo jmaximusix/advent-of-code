e800cf58e1582aa5110bf5c9cf66de4b48f26020
JMaximusIX <nix@gibts>
2023-12-09 06:57:59 +0100

    reverse instead of Part 1 / 2 cases

--- Year2023/Day09.hs
+++ Year2023/Day09.hs
@@ -1,19 +1,15 @@
 module Day09 (part1, part2) where
 
-import Advent (Part (Part1, Part2))
-
 part1, part2 :: [String] -> Int
-part1 = sum . map (predict Part1 . parseInput)
-part2 = sum . map (predict Part2 . parseInput)
+part1 = sum . map (predict . parseInput)
+part2 = sum . map (predict . reverse . parseInput)
 
 parseInput :: String -> [Int]
 parseInput = map read . words
 
-predict :: Part -> [Int] -> Int
-predict part xs
-  | all (== 0) diffs = edgeElement
-  | otherwise = edgeElement `plusminus` predict part diffs
+predict :: [Int] -> Int
+predict xs
+  | all (== 0) diffs = head xs
+  | otherwise = head xs + predict diffs
   where
     diffs = zipWith (-) (tail xs) xs
-    edgeElement = if part == Part1 then last xs else head xs
-    plusminus = if part == Part1 then (+) else (-)
