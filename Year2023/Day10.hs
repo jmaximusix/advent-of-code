7b899e258227f004f03fbbc29b15159ec3800741
JMaximusIX <nix@gibts>
2023-12-10 10:12:08 +0100

    working

--- Year2023/Day10.hs
+++ Year2023/Day10.hs
@@ -2,7 +2,7 @@
 
 module Day10 (part1, part2) where
 
-import Data.List (elemIndex, find, nub)
+import Data.List (elemIndex, find, nub, tails)
 import Data.List.Extra (chunksOf)
 import Data.Maybe (catMaybes, fromJust, isJust, isNothing)
 import Data.Tuple (swap)
@@ -15,6 +15,7 @@ type Grid = [[Char]]
 
 part1, part2 :: Grid -> String
 part1 grid = show $ walkInLoop 0 grid (traceShowId $ start grid) `div` 2
+
 -- part2 grid = traceShowId (length grid * length (head grid)) - traceShowId (length loop) - (traceShowId $ length (traceShowId $ notEnclosed grid loop [(0, 0)]) + 1)
 --   where
 --     loop = traceShowId $ traceLoop grid (start grid)
@@ -22,12 +23,12 @@ part1 grid = show $ walkInLoop 0 grid (traceShowId $ start grid) `div` 2
 --   where
 -- part2 grid = show $ pred $ length $ filter snd mapped
 
-part2 grid = pprint loop grid
-  where
-    mapped = traceShowId $ map (second (isEnclosed grid loop [])) yeet
-    yeet = map (first (snd . head) . dupe . pure) ps :: [(Pos, [(Bool, Pos)])]
-    loop = traceLoop grid (start grid)
-    ps = map (False,) $ filter (`notElem` loop) [(x, y) | y <- [0 .. length grid - 1], x <- [0 .. length (head grid) - 1]]
+-- part2 grid = pprint loop grid
+--   where
+--     mapped = traceShowId $ map (second (isEnclosed grid loop [])) yeet
+--     yeet = map (first (snd . head) . dupe . pure) ps :: [(Pos, [(Bool, Pos)])]
+--     loop = traceLoop grid (start grid)
+--     ps = map (False,) $ filter (`notElem` loop) [(x, y) | y <- [0 .. length grid - 1], x <- [0 .. length (head grid) - 1]]
 
 walkInLoop :: Int -> Grid -> (Pos, Pos) -> Int
 walkInLoop n grid p
@@ -36,33 +37,54 @@ walkInLoop n grid p
   where
     p' = next grid p
 
-isEnclosed :: Grid -> [Pos] -> [Pos] -> [(Bool, Pos)] -> Bool
-isEnclosed _ _ _ [] = True
-isEnclosed grid loop done ps
-  | isJust isOut = traceShow done $ fromJust isOut
-  | otherwise = isEnclosed grid loop (map snd yeet ++ done) yeet
-  where
-    yeet = filter ((`notElem` done) . snd) $ nextBfsAll grid loop ps
-    isOut = fst <$> find (isOutside grid . snd) yeet
+-- isEnclosed :: Grid -> [Pos] -> [Pos] -> [(Bool, Pos)] -> Bool
+-- isEnclosed _ _ _ [] = True
+-- isEnclosed grid loop done ps
+--   | isJust isOut = traceShow done $ fromJust isOut
+--   | otherwise = isEnclosed grid loop (map snd yeet ++ done) yeet
+--   where
+--     yeet = filter ((`notElem` done) . snd) $ nextBfsAll grid loop ps
+--     isOut = fst <$> find (isOutside grid . snd) yeet
 
-nextBfsAll :: Grid -> [Pos] -> [(Bool, Pos)] -> [(Bool, Pos)]
-nextBfsAll grid loop ps = nub $ concatMap (nextBfs grid loop) ps
+-- nextBfsAll :: Grid -> [Pos] -> [(Bool, Pos)] -> [(Bool, Pos)]
+-- nextBfsAll grid loop ps = nub $ concatMap (nextBfs grid loop) ps
 
-nextBfs :: Grid -> [Pos] -> (Bool, Pos) -> [(Bool, Pos)]
-nextBfs grid loop (skip, (x, y)) = catMaybes [left, right, up, down, leftdown, rightdown, leftup, rightup]
+part2 grid = show $ length $ filter id $ concat mapped
   where
-    left = if (x - 1, y) `elem` loop then left2 else Just (skip, (x - 1, y))
-    right = if (x + 1, y) `elem` loop then right2 else Just (skip, (x + 1, y))
-    up = if (x, y - 1) `elem` loop then up2 else Just (skip, (x, y - 1))
-    down = if (x, y + 1) `elem` loop then down2 else Just (skip, (x, y + 1))
-    leftdown = if (x - 1, y + 1) `elem` loop then Nothing else Just (skip, (x - 1, y + 1))
-    rightdown = if (x + 1, y + 1) `elem` loop then Nothing else Just (skip, (x + 1, y + 1))
-    leftup = if (x - 1, y - 1) `elem` loop then Nothing else Just (skip, (x - 1, y - 1))
-    rightup = if (x + 1, y - 1) `elem` loop then Nothing else Just (skip, (x + 1, y - 1))
-    left2 = if (x - 2, y) `elem` loop then Nothing else Just (not skip, (x - 2, y))
-    right2 = if (x + 2, y) `elem` loop then Nothing else Just (not skip, (x + 2, y))
-    up2 = if (x, y - 2) `elem` loop then Nothing else Just (not skip, (x, y - 2))
-    down2 = if (x, y + 2) `elem` loop then Nothing else Just (not skip, (x, y + 2))
+    mapped = map (map (traceShowId . isEnclosed2) . tails) grid'
+    grid' = pprint2 loop grid
+    loop = traceLoop grid (start grid)
+
+isEnclosed2 :: String -> Bool
+isEnclosed2 s | traceShow s False = undefined
+isEnclosed2 [] = False
+isEnclosed2 (x : xs)
+  | x /= '.' = False
+  | even $ traceShowId (((ups - downs) `div` 2) + reals) = False
+  | otherwise = True
+  where
+    ups = length $ filter hasVerticalUp xs
+    downs = length $ filter hasVerticalDown xs
+    reals = length $ filter hasRealVertical xs
+    hasRealVertical c = c `elem` ['|', 'S']
+    hasVerticalUp c = c `elem` ['J', 'F']
+    hasVerticalDown c = c `elem` ['L', '7']
+
+-- nextBfs :: Grid -> [Pos] -> (Bool, Pos) -> [(Bool, Pos)]
+-- nextBfs grid loop (skip, (x, y)) = catMaybes [left, right, up, down, leftdown, rightdown, leftup, rightup]
+--   where
+--     left = if (x - 1, y) `elem` loop then left2 else Just (skip, (x - 1, y))
+--     right = if (x + 1, y) `elem` loop then right2 else Just (skip, (x + 1, y))
+--     up = if (x, y - 1) `elem` loop then up2 else Just (skip, (x, y - 1))
+--     down = if (x, y + 1) `elem` loop then down2 else Just (skip, (x, y + 1))
+--     leftdown = if (x - 1, y + 1) `elem` loop then Nothing else Just (skip, (x - 1, y + 1))
+--     rightdown = if (x + 1, y + 1) `elem` loop then Nothing else Just (skip, (x + 1, y + 1))
+--     leftup = if (x - 1, y - 1) `elem` loop then Nothing else Just (skip, (x - 1, y - 1))
+--     rightup = if (x + 1, y - 1) `elem` loop then Nothing else Just (skip, (x + 1, y - 1))
+--     left2 = if (x - 2, y) `elem` loop then Nothing else Just (not skip, (x - 2, y))
+--     right2 = if (x + 2, y) `elem` loop then Nothing else Just (not skip, (x + 2, y))
+--     up2 = if (x, y - 2) `elem` loop then Nothing else Just (not skip, (x, y - 2))
+--     down2 = if (x, y + 2) `elem` loop then Nothing else Just (not skip, (x, y + 2))
 
 isOutside :: Grid -> Pos -> Bool
 isOutside grid (x, y) = x < 0 || y < 0 || y >= length grid || x >= length (head grid)
@@ -128,5 +150,10 @@ getSym g (x, y)
 
 pprint :: [Pos] -> Grid -> String
 pprint loop grid = ('\n' :) $ unlines $ chunksOf (length (head grid)) $ map (\x -> if x `elem` loop then '#' else '.') allps
+  where
+    allps = [(x, y) | y <- [0 .. length grid - 1], x <- [0 .. length (head grid) - 1]]
+
+pprint2 :: [Pos] -> Grid -> Grid
+pprint2 loop grid = chunksOf (length (head grid)) $ map (\p@(x, y) -> if p `elem` loop then grid !! y !! x else '.') allps
   where
     allps = [(x, y) | y <- [0 .. length grid - 1], x <- [0 .. length (head grid) - 1]]
