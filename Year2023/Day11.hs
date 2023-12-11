73e2f034ec8b5868fcf27fe166429212102b2766
JMaximusIX <nix@gibts>
2023-12-11 07:57:19 +0100

    Day 11 beautification

--- Year2023/Day11.hs
+++ Year2023/Day11.hs
@@ -1,46 +1,56 @@
 module Day11 (part1, part2) where
 
 import Combinatorics (tuples)
-import Data.Bifunctor (Bifunctor (second), bimap)
+import Data.Bifunctor (bimap)
 import Data.List (transpose)
 import qualified Data.Set as Set (Set, fromList, map, toList)
-import Data.Tuple.Extra (both, dupe, swap)
+import Data.Tuple.Extra (both, dupe, second, swap)
 import Geometry (Grid, Pos, pointList, tcabDist)
 
 part1, part2 :: Grid Char -> Int
-part1 = sum . map (\[a, b] -> tcabDist a b) . tuples 2 . Set.toList . expandUniverse 1 . parseInput
-part2 = sum . map (\[a, b] -> tcabDist a b) . tuples 2 . Set.toList . expandUniverse 999999 . parseInput
+part1 = solve 1
+part2 = solve 999999
+
+solve :: Int -> Grid Char -> Int
+solve expFactor =
+  sum
+    . map (\[a, b] -> tcabDist a b)
+    . tuples 2
+    . Set.toList
+    . expandUniverse expFactor
+    . parseInput
+
+isStar :: Char -> Bool
+isStar = (== '#')
 
 parseInput :: Grid Char -> (Set.Set Pos, ([Int], [Int]))
 parseInput =
   bimap
     ( Set.fromList
         . map snd
-        . filter (\(x, _) -> x == '#')
+        . filter (isStar . fst)
         . uncurry zip
         . bimap concat pointList
     )
-    (both getEmptyRows . second transpose)
+    ( both
+        (map fst . filter (not . any isStar . snd) . zip [0 ..])
+        . second transpose
+    )
     . both dupe
     . dupe
 
-getEmptyRows :: Grid Char -> [Int]
-getEmptyRows = map fst . filter (\(_, row) -> all (== '.') row) . zip [0 ..]
-
 expandUniverse :: Int -> (Set.Set Pos, ([Int], [Int])) -> Set.Set Pos
-expandUniverse expFactor (s, empty) =
+expandUniverse expFactor (set, empty) =
   foldl
     (expandDimension expFactor swap)
-    (foldl (expandDimension expFactor id) s emptyColumns)
+    (foldl (expandDimension expFactor id) set emptyColumns)
     emptyRows
   where
     (emptyRows, emptyColumns) = both (zipWith (+) [0, expFactor ..]) empty
 
 expandDimension :: Int -> (Pos -> Pos) -> Set.Set Pos -> Int -> Set.Set Pos
-expandDimension factor f set rowOrCol = Set.map expand set
+expandDimension expFactor f set rowOrCol = Set.map expand set
   where
-    expand p
-      | a > rowOrCol = f (a + factor, b)
-      | otherwise = p
-      where
-        (a, b) = f p
+    expand p =
+      let (a, b) = f p
+       in if a > rowOrCol then f (a + expFactor, b) else p
