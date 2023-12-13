cdcacd22cf522195643f91381c08b1c26208905e
JMaximusIX <nix@gibts>
2023-12-13 03:28:32 +0100

    Day 12 better

--- Year2023/Day12.hs
+++ Year2023/Day12.hs
@@ -5,66 +5,61 @@ module Day12 (part1, part2) where
 import Data.Bifunctor (bimap)
 import Data.List (elemIndex, intercalate, tails)
 import Data.List.Extra (groupOn, splitOn)
-import qualified Data.Map as Map (Map, empty, filterWithKey, fromList, fromListWith, map, mapKeys, toList, unionWith, unionsWith)
+import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
-import Debug.Trace (traceShowId)
+
+data Status = Unknown | Broken | Good deriving (Eq, Ord)
+
+type Sequence = [Status]
 
 part1, part2 :: [String] -> Int
-part1 input = sum $ map (solveLine . parseInput) input
-part2 input = sum $ map (solveLine . bimap (intercalate [Unknown] . replicate 5) (concat . replicate 5) . parseInput) input
+part1 = sum . map (solveLine . parseInput)
+part2 = sum . map (solveLine . resize . parseInput)
+  where
+    resize =
+      bimap
+        (intercalate [Unknown] . replicate 5)
+        (concat . replicate 5)
 
-solveLine :: ([Info], [Int]) -> Int
-solveLine (is, r) = alignall (Map.fromList [(is, 1)]) (r ++ [0])
+solveLine :: (Sequence, [Int]) -> Int
+solveLine (is, r) = alignAll (Map.fromList [(is, 1)]) r
 
-isBrokenAt :: Int -> [Info] -> Bool
+isBrokenAt :: Int -> Sequence -> Bool
 isBrokenAt i is
   | i >= length is || i < 0 = False
   | otherwise = is !! i == Broken
 
-alignall :: Map.Map [Info] Int -> [Int] -> Int
-alignall m [0] = sum $ Map.filterWithKey (\k _ -> Broken `notElem` k) m
-alignall m (a : b : rest) = alignall yeet (b : rest)
+alignAll :: Map.Map Sequence Int -> [Int] -> Int
+alignAll m [] = sum $ Map.filterWithKey (\k _ -> Broken `notElem` k) m
+alignAll m (a : rest) = alignAll (Map.unionsWith (+) next) rest
   where
-    map' = Map.toList m
-    soos = map (\(is, count) -> Map.map (* count) $ alignFirst is a b) map'
-    yeet = Map.unionsWith (+) soos
+    next = map (\(is, count) -> Map.map (* count) $ alignFirst is a) $ Map.toList m
 
-alignFirst :: [Info] -> Int -> Int -> Map.Map [Info] Int
-alignFirst is this next = Map.mapKeys (++ rest) soos
+alignFirst :: Sequence -> Int -> Map.Map Sequence Int
+alignFirst is size = Map.mapKeys (++ rest) possiblePlacements
   where
-    firstBroken = fromMaybe (length is) (elemIndex Broken is) + this + 1
+    firstBroken = fromMaybe (length is) (elemIndex Broken is) + size + 1
     (relevant, rest) = splitAt firstBroken is
     grouped = groupOn (== Good) relevant
-    soos = snd $ foldl (foldlGrouped is this next) (0, Map.empty) $ init $ tails grouped
+    possiblePlacements = foldl (place size) Map.empty $ init $ tails grouped
 
-foldlGrouped :: [Info] -> Int -> Int -> (Int, Map.Map [Info] Int) -> [[Info]] -> (Int, Map.Map [Info] Int)
-foldlGrouped total this next (n, remains) (xs : rest)
-  | head xs == Good || l < this = (n + l, remains)
-  | otherwise = (n + l, Map.unionWith (+) remains genOptions)
+place :: Int -> Map.Map Sequence Int -> [Sequence] -> Map.Map Sequence Int
+place size remains (xs : rest)
+  | head xs == Good || l < size = remains
+  | otherwise = Map.unionWith (+) remains genOptions
   where
     l = length xs
-    mayNotUseLast = if (l > this) && isBrokenAt (l - this - 1) xs then 1 else 0
+    mayNotUseLast = if isBrokenAt (l - size - 1) xs then 1 else 0
     genOptions =
       Map.fromListWith (+) $
-        map ((,1 :: Int) . concat . trytrunc next . (: rest) . (`drop` xs) . (+ 1)) $
-          filter (\n' -> not $ isBrokenAt (n + n') total) [this .. l - mayNotUseLast]
+        map ((,1) . concat . (: rest) . (`drop` xs) . (+ 1)) $
+          filter (\n' -> not $ isBrokenAt n' xs) [size .. l - mayNotUseLast]
 
-trytrunc :: Int -> [[Info]] -> [[Info]]
-trytrunc n stuff = dropWhile (\xs -> notElem Broken xs && length xs < n) (init stuff) ++ [last stuff]
-
-data Info = Unknown | Broken | Good deriving (Eq, Ord)
-
-instance Show Info where
-  show Unknown = "?"
-  show Broken = "#"
-  show Good = "."
-
-charToInfo :: Char -> Info
-charToInfo '#' = Broken
-charToInfo '.' = Good
-charToInfo '?' = Unknown
-
-parseInput :: String -> ([Info], [Int])
+parseInput :: String -> (Sequence, [Int])
 parseInput str = (map charToInfo info, map read $ splitOn "," rules)
   where
     [info, rules] = words str
+    charToInfo c = case c of
+      '#' -> Broken
+      '.' -> Good
+      '?' -> Unknown
