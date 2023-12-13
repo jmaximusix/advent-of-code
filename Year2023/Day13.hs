8363d6ae4ba33b32ad694ab3d634dfccccaa764c
JMaximusIX <nix@gibts>
2023-12-13 07:42:17 +0100

    Day 13

--- Year2023/Day13.hs
+++ Year2023/Day13.hs
@@ -1,5 +1,38 @@
 module Day13 (part1, part2) where
 
-part1, part2 :: [String] -> Int
-part1 = undefined
-part2 = undefined
+import Data.List.Extra (splitOn, transpose)
+import Data.Maybe (fromJust, mapMaybe)
+import Geometry (Grid)
+
+part1, part2 :: Grid Char -> Int
+part1 = sum . map (checkAllSymmetries 0) . parseInput
+part2 = sum . map (checkAllSymmetries 1) . parseInput
+
+parseInput :: Grid Char -> [Grid Char]
+parseInput = splitOn [""]
+
+checkAllSymmetries :: Int -> Grid Char -> Int
+checkAllSymmetries n g = maybe (fromJust horizontal) (100 *) vertical
+  where
+    (vertical, horizontal) = (verticalSymmetry n g, verticalSymmetry n (transpose g))
+
+verticalSymmetry :: Int -> Grid Char -> Maybe Int
+verticalSymmetry n g
+  | bottom > top = (length g -) <$> bottom
+  | otherwise = top
+  where
+    top = verticalSymmetryFromTop n g
+    bottom = verticalSymmetryFromTop n (reverse g)
+
+verticalSymmetryFromTop :: Int -> Grid Char -> Maybe Int
+verticalSymmetryFromTop n g
+  | null works = Nothing
+  | otherwise = Just $ last works
+  where
+    works = mapMaybe hasSymmetry [1 .. (length g `div` 2)]
+    hasSymmetry i
+      | differInExactlyN n (take i g) (reverse (take i (drop i g))) = Just i
+      | otherwise = Nothing
+
+differInExactlyN :: Int -> Grid Char -> Grid Char -> Bool
+differInExactlyN n a b = length (filter id $ zipWith (/=) (concat a) (concat b)) == n
