de59f7e36559acd433634520dc87b43bc0187233
jmaximusix <jmaximusix@gmail.com>
2024-12-01 18:16:47 +0100

    4 characters longer, but better readability

--- Year2024/Day01.hs
+++ Year2024/Day01.hs
@@ -4,7 +4,7 @@ import Data.List (sort)
 import Data.Tuple.Extra (both)
 
 part1, part2 :: [String] -> Int
-part1 = sum . uncurry (zipWith ((abs .) . (-))) . both sort . parseInput
+part1 = sum . map (abs . uncurry (-)) . uncurry zip . both sort . parseInput
 part2 = (\(l, r) -> sum $ map (\l' -> length (filter (== l') r) * l') l) . parseInput
 
 parseInput :: [String] -> ([Int], [Int])
