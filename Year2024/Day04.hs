2a875e8397b49b5ba56fa54a2195e086e1bed2df
jmaximusix <jmaximusix@gmail.com>
2024-12-04 08:47:39 +0100

    nicer code

--- Year2024/Day04.hs
+++ Year2024/Day04.hs
@@ -1,34 +1,31 @@
 module Day04 (part1, part2) where
 
+import Control.Applicative (liftA2)
 import Data.List (transpose)
-import Data.Set (Set, member)
-import Debug.Trace (traceShow, traceShowId)
-import Geometry (Grid, Pos, getGridElement, getGridElementWithDefault, pointList)
-import Text.Regex.TDFA
+import qualified Data.Set as Set (fromList)
+import Data.Tuple.Extra (both)
+import Geometry (Grid, Pos, dimensions, getGridElement, getGridElementSafe, pointList)
+import Text.Regex.TDFA ((=~))
 
-part1, part2 :: [String] -> Int
-part1 s = sum (map countXmas s) + sum (map countXmas (map reverse s)) + sum (map countXmas (transpose s)) + sum (map countXmas (map reverse (transpose s))) + sum (map countXmas (diagonals s)) + sum (map countXmas (map reverse (diagonals s)))
-part2 s = length $ filter (posMas s) (pointList s)
+part1, part2 :: [[Char]] -> Int
+part1 input = countXmas input + countXmas (transpose input) + countXmas (diagonals input)
+part2 input = length $ filter (posMas input) (pointList input)
 
-countXmas :: String -> Int
-countXmas input = input =~ "XMAS" :: Int
+countXmas :: [String] -> Int
+countXmas = sum . map (\l -> (l =~ "XMAS" :: Int) + (reverse l =~ "XMAS" :: Int))
+
+diagonals :: [[a]] -> [[a]]
+diagonals g = mainDiags g ++ mainDiags (reverse g)
+  where
+    (w, h) = both (subtract 1) (dimensions g)
+    mainDiags g' = [[g' !! a !! (n - a) | a <- [0 .. min n w], (n - a) < h + 1] | n <- [0 .. w + h]]
 
 posMas :: Grid Char -> Pos -> Bool
 posMas g pos@(x, y)
-  | c /= 'A' = False
-  | otherwise = ((lu == 'M' && rd == 'S') || (rd == 'M' && lu == 'S')) && ((ru == 'M' && ld == 'S') || (ld == 'M' && ru == 'S'))
+  | getGridElement g pos /= 'A' = False
+  | otherwise = diag ((x + 1, y + 1), (x - 1, y - 1)) && diag ((x + 1, y - 1), (x - 1, y + 1))
   where
-    c = getGridElement g pos
-    lu = getGridElementWithDefault ' ' g (x - 1, y - 1)
-    ru = getGridElementWithDefault ' ' g (x + 1, y - 1)
-    ld = getGridElementWithDefault ' ' g (x - 1, y + 1)
-    rd = getGridElementWithDefault ' ' g (x + 1, y + 1)
-
-diagonals :: [[a]] -> [[a]]
-diagonals [] = []
-diagonals xss = mainDiags ++ antiDiags
-  where
-    n = length xss
-    m = length (head xss)
-    mainDiags = [[xss !! (i - k) !! k | k <- [0 .. min i (m - 1)], i - k < n] | i <- [0 .. n + m - 2]]
-    antiDiags = [[xss !! (i - k) !! (m - 1 - k) | k <- [0 .. min i (m - 1)], i - k < n] | i <- [0 .. n + m - 2]]
+    diag =
+      elem (Set.fromList ['S', 'M'])
+        . uncurry (liftA2 (\a b -> Set.fromList [a, b]))
+        . both (getGridElementSafe g)
