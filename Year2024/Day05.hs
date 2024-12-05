fa8e6c8b5605425e9575b7965950d4d0a996d50e
jmaximusix <jmaximusix@gmail.com>
2024-12-05 07:05:26 +0100

    switched to Ints instead of strings

--- Year2024/Day05.hs
+++ Year2024/Day05.hs
@@ -4,33 +4,33 @@ import Combinatorics (tuples)
 import Data.List.Extra (replace, splitOn)
 import Debug.Trace (traceShow, traceShowId)
 
-part1, part2 :: [String] -> Int
-part1 input = sum . map fst . traceShowId . filter (\(_, b) -> not (any (violates rules) b)) $ traceShowId $ map ((\x -> (read (middleItem x), tuples 2 x)) . splitOn ",") orders
-  where
-    (rules, orders) = parseInput input
--- part1 input = traceShow (map (tuples 2 . splitOn ",") orders) 1
---   where
---     (rules, orders) = parseInput input
-part2 input = sum . map (read . middleItem . reorder rules . fst) . traceShowId . filter (\(_, b) -> any (violates rules) b) $ traceShowId $ map ((\x -> (x, tuples 2 x)) . splitOn ",") orders
-  where
-    (rules, orders) = parseInput input
+type Rule = (Int, Int)
 
-parseInput :: [String] -> ([String], [String])
-parseInput input = (a, b)
+part1, part2 :: [String] -> Int
+part1 input = sum . map fst . traceShowId . filter (\(_, b) -> not (any (violatesRules rules) b)) $ traceShowId $ map (\x -> (middleItem x, map (\[a, b] -> (a, b)) $ tuples 2 x)) updates
   where
+    (rules, updates) = parseInput input
+part2 input = sum . map (middleItem . reorder rules . fst) . traceShowId . filter (\(_, b) -> any (violatesRules rules) b) $ traceShowId $ map (\x -> (x, map (\[a, b] -> (a, b)) $ tuples 2 x)) updates
+  where
+    (rules, updates) = parseInput input
+
+parseInput :: [String] -> ([Rule], [[Int]])
+parseInput input = (rules, map (map read . splitOn ",") b)
+  where
+    rules = map ((\[p, q] -> (p, q)) . map read . splitOn "|") a
     [a, b] = splitOn [""] input
 
 middleItem :: [a] -> a
 middleItem xs = xs !! (length xs `div` 2)
 
-violates :: [String] -> [String] -> Bool
-violates rules [a, b] = (b ++ "|" ++ a) `elem` rules
+violatesRules :: [Rule] -> (Int, Int) -> Bool
+violatesRules rules (a, b) = (b, a) `elem` rules
 
-reorder :: [String] -> [String] -> [String]
+reorder :: [Rule] -> [Int] -> [Int]
 reorder _ [] = []
 reorder rules (a : bs)
   | null l = a : reorder rules bs
   | otherwise = reorder rules (y : replace [y] [x] bs)
   where
-    l = filter (violates rules) [[a, n] | n <- bs]
-    [x, y] = head l
+    l = filter (violatesRules rules) [(a, n) | n <- bs]
+    (x, y) = head l
