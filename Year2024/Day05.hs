fd9fd05450c35b3040e16a22e367df4190578ecc
jmaximusix <jmaximusix@gmail.com>
2024-12-05 07:32:31 +0100

    use sortBy

--- Year2024/Day05.hs
+++ Year2024/Day05.hs
@@ -1,8 +1,8 @@
 module Day05 (part1, part2) where
 
 import Combinatorics (tuples)
-import Data.List (partition)
-import Data.List.Extra (replace, splitOn)
+import Data.List (partition, sortBy)
+import Data.List.Extra (splitOn)
 import MyLib (tup2)
 
 type Rule = (Int, Int)
@@ -11,7 +11,7 @@ type Update = [Int]
 
 part1, part2 :: [String] -> Int
 part1 = sum . map middleItem . snd . uncurry partitionUpdates . parseInput
-part2 input = sum . map (middleItem . reorder rules) . fst $ partitionUpdates rules updates
+part2 input = sum . map (middleItem . sortBy (sortHelper rules)) . fst $ partitionUpdates rules updates
   where
     (rules, updates) = parseInput input
 
@@ -30,11 +30,8 @@ middleItem xs = xs !! (length xs `div` 2)
 violatesRules :: [Rule] -> (Int, Int) -> Bool
 violatesRules rules (a, b) = (b, a) `elem` rules
 
-reorder :: [Rule] -> Update -> Update
-reorder _ [] = []
-reorder rules (a : bs)
-  | null l = a : reorder rules bs
-  | otherwise = reorder rules (y : replace [y] [x] bs)
-  where
-    l = filter (violatesRules rules) [(a, n) | n <- bs]
-    (x, y) = head l
+sortHelper :: [Rule] -> Int -> Int -> Ordering
+sortHelper rules a b
+  | violatesRules rules (a, b) = GT
+  | violatesRules rules (b, a) = LT
+  | otherwise = EQ
