d0660bb8745693fc18d269006cb6803badd9bc12
jmaximusix <jmaximusix@gmail.com>
2024-12-05 08:11:28 +0100

    all sorting stuff

--- Year2024/Day05.hs
+++ Year2024/Day05.hs
@@ -1,28 +1,24 @@
 module Day05 (part1, part2) where
 
-import Combinatorics (tuples)
-import Data.List (partition, sortBy)
+import Data.List (sortBy)
 import Data.List.Extra (splitOn)
+import GHC.Utils.Misc (isSortedBy)
 import MyLib (tup2)
 
 type Rule = (Int, Int)
 
-type Update = [Int]
-
 part1, part2 :: [String] -> Int
-part1 = sum . map middleItem . snd . uncurry partitionUpdates . parseInput
-part2 input = sum . map (middleItem . sortBy (sortHelper rules)) . fst $ partitionUpdates rules updates
+part1 = sum . map middleItem . uncurry (filter . isSortedBy . ruleOrder) . parseInput
+part2 input = sum . map (middleItem . sortBy f) $ filter (not . isSortedBy f) updates
   where
     (rules, updates) = parseInput input
+    f = ruleOrder rules
 
-parseInput :: [String] -> ([Rule], [Update])
+parseInput :: [String] -> ([Rule], [[Int]])
 parseInput input = (rules, map (map read . splitOn ",") b)
   where
     rules = map (tup2 . map read . splitOn "|") a
     [a, b] = splitOn [""] input
-
-partitionUpdates :: [Rule] -> [Update] -> ([Update], [Update])
-partitionUpdates rules = partition (any (violatesRules rules . tup2) . tuples 2)
 
 middleItem :: [a] -> a
 middleItem xs = xs !! (length xs `div` 2)
@@ -30,8 +26,8 @@ middleItem xs = xs !! (length xs `div` 2)
 violatesRules :: [Rule] -> (Int, Int) -> Bool
 violatesRules rules (a, b) = (b, a) `elem` rules
 
-sortHelper :: [Rule] -> Int -> Int -> Ordering
-sortHelper rules a b
+ruleOrder :: [Rule] -> Int -> Int -> Ordering
+ruleOrder rules a b
   | violatesRules rules (a, b) = GT
   | violatesRules rules (b, a) = LT
   | otherwise = EQ
