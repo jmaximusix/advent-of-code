8135003edcf7baa981e3fdba5e4e4bfac3c547b4
jmaximusix <jmaximusix@gmail.com>
2024-12-06 07:29:24 +0100

    combine part1 and part2 to exitPath function

--- Year2024/Day06.hs
+++ Year2024/Day06.hs
@@ -1,33 +1,27 @@
 module Day06 (part1, part2) where
 
-import Data.Maybe (isNothing)
-import qualified Data.Set as Set (Set, fromList, insert, member)
-import Geometry (Direction (R, U), Grid, Pos, getGridElementSafe, goNSteps, index2d, replace2d, turn, zipPoints)
+import Data.Maybe (fromJust, isNothing)
+import qualified Data.Set as Set (Set, filter, fromList, insert, map, member)
+import Geometry (Direction (R, U), Grid, Pos, dimensions, getGridElementSafe, goNSteps, index2d, isInside, replace2d, turn)
 
 part1, part2 :: Grid Char -> Int
-part1 input = moveUntilEnd (start, U, Set.fromList [start]) input
+part1 grid = length $ fromJust $ exitPath (start, Set.fromList [start]) grid
   where
-    start = index2d '^' input
-part2 grid = length $ filter (moveUntilLoop (start, U, Set.fromList [(start, U)]) . placeObstacleAt) possibleNewObstacles
+    start = (index2d '^' grid, U)
+part2 grid = length $ Set.filter (isNothing . exitPath (start, Set.fromList [start]) . placeObstacleAt) possibleNewObstacles
   where
     placeObstacleAt p = replace2d p '#' grid
-    possibleNewObstacles = map fst . filter (\(_, b) -> b == '.') . zipPoints $ grid
-    start = index2d '^' grid
+    possibleNewObstacles = fromJust $ exitPath (start, Set.fromList [start]) grid
+    start = (index2d '^' grid, U)
 
-moveUntilEnd :: (Pos, Direction, Set.Set Pos) -> Grid Char -> Int
-moveUntilEnd (loc, dir, visited) g
-  | isNothing (getGridElementSafe g loc') = length visited
-  | otherwise = moveUntilEnd (loc', dir', loc' `Set.insert` visited) g
+-- Just (Set of visited positions) | Nothing (Guard is stuck in loop)
+exitPath :: ((Pos, Direction), Set.Set (Pos, Direction)) -> Grid Char -> Maybe (Set.Set Pos)
+exitPath (current, visited) g
+  | (not . isInside (dimensions g)) loc' = Just (Set.map fst visited)
+  | new `Set.member` visited = Nothing
+  | otherwise = exitPath (new, new `Set.insert` visited) g
   where
-    (loc', dir') = moveGuard (loc, dir) g
-
-moveUntilLoop :: (Pos, Direction, Set.Set (Pos, Direction)) -> Grid Char -> Bool
-moveUntilLoop (loc, dir, visited) g
-  | isNothing (getGridElementSafe g loc') = False
-  | new `Set.member` visited = True
-  | otherwise = moveUntilLoop (loc', dir', new `Set.insert` visited) g
-  where
-    new@(loc', dir') = moveGuard (loc, dir) g
+    new@(loc', _) = moveGuard current g
 
 moveGuard :: (Pos, Direction) -> Grid Char -> (Pos, Direction)
 moveGuard (loc, dir) g
