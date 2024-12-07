5d1cf31489882ead898bf3a45eaab78303174181
jmaximusix <jmaximusix@gmail.com>
2024-12-07 07:12:13 +0100

    Day 7 final cleanup

--- Year2024/Day07.hs
+++ Year2024/Day07.hs
@@ -1,34 +1,23 @@
 module Day07 (part1, part2) where
 
+import Advent (Part (Part1, Part2))
 import Data.List (isSuffixOf)
-import Debug.Trace (traceShow, traceShowId)
-import Geometry (Grid, Pos, index2d, replace2d)
 
 part1, part2 :: [String] -> Int
-part1 = sum . map (\x -> read $ init (head (words x))) . filter canBeTrue
-part2 = sum . map (\x -> read $ init (head (words x))) . filter canBeTrue
+part1 = sum . map fst . filter (uncurry (canCreateFrom Part1)) . map parseInput
+part2 = sum . map fst . filter (uncurry (canCreateFrom Part2)) . map parseInput
 
-canBeTrue :: String -> Bool
-canBeTrue input = canCreateFrom res' nums'
-  where
-    (res : nums) = words input
-    nums' = reverse $ map read nums
-    res' = read $ init res
+parseInput :: String -> (Int, [Int])
+parseInput = (\(r : ns) -> (read (init r), reverse (map read ns))) . words
 
-canCreateFrom :: Int -> [Int] -> Bool
-canCreateFrom res [x] = res == x
-canCreateFrom res (n : ns)
-  | canCreateFrom (res - n) ns = True
-  | res `mod` n == 0 && canCreateFrom (res `div` n) ns = True
-  | n' `isSuffixOf` res' = canCreateFrom (removeSuffix res' n') ns
+canCreateFrom :: Part -> Int -> [Int] -> Bool
+canCreateFrom _ res [n] = res == n
+canCreateFrom p res (n : ns)
+  | canCreateFrom p (res - n) ns = True
+  | res `mod` n == 0 && canCreateFrom p (res `div` n) ns = True
+  | p == Part2 && (n' `isSuffixOf` tail r') = canCreateFrom p unconcat ns
   | otherwise = False
   where
-    res' = show res
+    unconcat = read (take (length r' - length n') r')
+    r' = show res
     n' = show n
-
-removeSuffix :: [Char] -> [Char] -> Int
-removeSuffix l suff
-  | null l' = 0
-  | otherwise = read l'
-  where
-    l' = take (length l - length suff) l
