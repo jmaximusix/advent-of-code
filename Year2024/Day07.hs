a03876b901cfeda12b22232e83b20171a6189078
jmaximusix <jmaximusix@gmail.com>
2024-12-07 06:48:48 +0100

    day 7 submit 2

--- Year2024/Day07.hs
+++ Year2024/Day07.hs
@@ -1,11 +1,12 @@
 module Day07 (part1, part2) where
 
+import Data.List (isSuffixOf)
 import Debug.Trace (traceShow, traceShowId)
 import Geometry (Grid, Pos, index2d, replace2d)
 
 part1, part2 :: [String] -> Int
 part1 = sum . map (\x -> read $ init (head (words x))) . filter canBeTrue
-part2 = undefined
+part2 = sum . map (\x -> read $ init (head (words x))) . filter canBeTrue
 
 canBeTrue :: String -> Bool
 canBeTrue input = canCreateFrom res' nums'
@@ -15,8 +16,19 @@ canBeTrue input = canCreateFrom res' nums'
     res' = read $ init res
 
 canCreateFrom :: Int -> [Int] -> Bool
-canCreateFrom res [a] = res == a
+canCreateFrom res [x] = res == x
 canCreateFrom res (n : ns)
   | canCreateFrom (res - n) ns = True
   | res `mod` n == 0 && canCreateFrom (res `div` n) ns = True
+  | n' `isSuffixOf` res' = canCreateFrom (removeSuffix res' n') ns
   | otherwise = False
+  where
+    res' = show res
+    n' = show n
+
+removeSuffix :: [Char] -> [Char] -> Int
+removeSuffix l suff
+  | null l' = 0
+  | otherwise = read l'
+  where
+    l' = take (length l - length suff) l
