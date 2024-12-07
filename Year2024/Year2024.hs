f99e171db9c13eab00a2025f263921fe26d3b4f2
jmaximusix <jmaximusix@gmail.com>
2024-12-07 07:40:13 +0100

    create modules for upcoming days

--- Year2024/Year2024.hs
+++ Year2024/Year2024.hs
@@ -10,14 +10,14 @@ import qualified Day05
 import qualified Day06
 import qualified Day07
 import qualified Day08
--- import qualified Day09
--- import qualified Day10
--- import qualified Day11
--- import qualified Day12
--- import qualified Day13
--- import qualified Day14
--- import qualified Day15
--- import qualified Day16
+import qualified Day09
+import qualified Day10
+import qualified Day11
+import qualified Day12
+import qualified Day13
+import qualified Day14
+import qualified Day15
+import qualified Day16
 -- import qualified Day17
 -- import qualified Day18
 -- import qualified Day19
