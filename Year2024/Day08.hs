d137a91b234ad31050bd27c39aa979e69671ead7
jmaximusix <jmaximusix@gmail.com>
2024-12-09 05:14:57 +0100

    minor refactoring

--- Year2024/Day08.hs
+++ Year2024/Day08.hs
@@ -10,23 +10,23 @@ import Linear (V2 (V2))
 import MyLib (tup2)
 
 part1, part2 :: Grid Char -> Int
-part1 = solve (\g a b -> filter (isInside g . toPos) [2 * b - a, 2 * a - b])
+part1 = solve (\g (a, b) -> filter (isInside g . toPos) [2 * b - a, 2 * a - b])
 part2 =
   solve
-    ( \g a b ->
+    ( \g (a, b) ->
         let d = toPos (b - a)
             filldir f = takeWhile (isInside g . toPos) (iterate (f ((uncurry V2 . both (`div` uncurry gcd d)) d)) b)
          in filldir (+) ++ filldir (flip (-))
     )
 
-solve :: (Grid Char -> V2 Int -> V2 Int -> [V2 Int]) -> Grid Char -> Int
+solve :: (Grid Char -> (V2 Int, V2 Int) -> [V2 Int]) -> Grid Char -> Int
 solve antennas g =
   length
     . foldl
       ( \s ->
           Set.union s
             . Set.fromList
-            . concatMap (uncurry (antennas g) . tup2)
+            . concatMap (antennas g . tup2)
             . variate 2
       )
       Set.empty
