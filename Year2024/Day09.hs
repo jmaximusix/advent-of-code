f255dc515408cd9429b33de2b8f1d0ab285c380e
jmaximusix <jmaximusix@gmail.com>
2024-12-09 10:19:44 +0100

    day9 part2 refactored

--- Year2024/Day09.hs
+++ Year2024/Day09.hs
@@ -1,64 +1,50 @@
 module Day09 (part1, part2) where
 
-import Data.List (delete, find, findIndex, inits, uncons)
+import Data.Bifunctor (bimap, second)
+import Data.List (find, findIndex, uncons)
 import Data.List.Extra (chunksOf, unsnoc)
-import Data.List.Split (splitWhen)
 import Data.Maybe (catMaybes, fromJust, fromMaybe, isJust, isNothing)
 import Debug.Trace (traceShow, traceShowId)
-import Geometry (Direction, Grid, Pos, getGridElementSafe, getGridElementWithDefault, goNSteps, index2d, replace2d, toPos, toV2, zipPoints)
 import MyLib (tup2)
 
--- fileid, length of file, free space following
-type File = (Int, (Int, Int))
+-- ((fileid, length of file), free space following)
+type File = ((Int, Int), Int)
 
 part1, part2 :: [String] -> Int
 part1 input = sum $ zipWith (*) [0 ..] $ fst reordered
   where
     filelist = parseInput (head input ++ "0")
-    freeSpace = sum $ map (snd . snd) filelist
+    freeSpace = sum $ map snd filelist
     stream = concatMap blockStream filelist
     splitPoint = length stream - freeSpace
     (before, after) = splitAt splitPoint stream
-    reordered = foldl foldFunc ([], catMaybes (reverse after)) before
-part2 input = sum $ zipWith (*) [0 ..] $ map (\x -> fromMaybe 0 x) $ concatMap blockStream reordered
+    reordered = foldl foldFunc ([], reverse (catMaybes after)) before
+part2 input = fst $ foldl calcRes (0, 0) reordered
   where
     filelist = parseInput (head input ++ "0")
-    reordered = foldl tryFit filelist (reverse filelist)
+    reordered = foldl tryFit filelist (reverse [0 .. length filelist - 1])
 
 parseInput :: String -> [File]
-parseInput = zip [0 ..] . map tup2 . chunksOf 2 . map (\x -> read [x])
+parseInput = zipWith (\i [l, s] -> ((i, l), s)) [0 ..] . chunksOf 2 . map (\x -> read [x])
 
 blockStream :: File -> [Maybe Int]
-blockStream (fileid, (len, freeSpace)) = replicate len (Just fileid) ++ replicate freeSpace Nothing
+blockStream ((fileid, len), freeSpace) = replicate len (Just fileid) ++ replicate freeSpace Nothing
 
 foldFunc :: ([Int], [Int]) -> Maybe Int -> ([Int], [Int])
 foldFunc (merged, backup) Nothing = (merged ++ [head backup], tail backup)
 foldFunc (merged, backup) (Just x) = (merged ++ [x], backup)
 
-tryFit :: [File] -> File -> [File]
--- tryFit files file | traceShow (file, files) False = undefined
-tryFit files f@(fid, _)
-  | succeeded = soos ++ [(id'', (l'', 0)), (id', (l', f'' - l' + offset))] ++ delrest
-  | otherwise = files
+tryFit :: [File] -> Int -> [File]
+tryFit files fid
+  | isNothing spaceidx = files
+  | fromJust spaceidx == fileidx - 1 = before' ++ [(sp, 0), (f, sps + fs)] ++ after
+  | otherwise = before' ++ [(sp, 0), (f, sps - fl)] ++ between' ++ [(bf, bfs + fl + fs)] ++ after
   where
-    file@(id', (l', _)) = fromJust $ find (\(id, _) -> id == fid) files
-    soos = takeWhile (\(id, (_, f)) -> f < l' && id' /= id) files
-    maybeyeet = uncons $ drop (length soos) files
-    (next@(id'', (l'', f'')), rest) = fromJust maybeyeet
-    succeeded = isJust maybeyeet && id'' /= id'
-    (delrest, offset) = deleteFileFromRest rest file
+    fileidx = fromJust $ findIndex (\((id', _), _) -> id' == fid) files
+    (before, (f@(_, fl), fs) : after) = splitAt fileidx files
+    spaceidx = findIndex (\s -> snd s >= fl) before
+    (before', (sp, sps) : between) = splitAt (fromJust spaceidx) before
+    (between', (bf, bfs)) = fromJust $ unsnoc between
 
-deleteFileFromRest :: [File] -> File -> ([File], Int)
-deleteFileFromRest [] _ = ([], 0)
--- deleteFileFromRest rest f | traceShow (rest, f) False = undefined
-deleteFileFromRest rest file@(id, (l, f))
-  | isNothing yeet = (rest, 0)
-  | isNothing $ unsnoc before = (after, f + l)
-  | otherwise = (before' ++ [(did, (dl, df + f + l))] ++ after, 0)
-  where
-    yeet = findIndex (\(id', _) -> id' == id) rest
-
-    soos = splitWhen (\(id', _) -> id' == id) rest
-    [before, after] = soos
-    -- [(bid, (bl, bf))] = head soos
-    (before', (did, (dl, df))) = fromJust $ unsnoc before
+calcRes :: (Int, Int) -> File -> (Int, Int)
+calcRes (acc, idx) ((f, l), s) = (acc + f * (idx * l + ((l * (l - 1)) `div` 2)), idx + l + s)
