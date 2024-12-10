b801321120c2c7acdcfb6543f79a2dee981ed534
jmaximusix <jmaximusix@gmail.com>
2024-12-10 07:01:11 +0100

    move nub inside for better efficiency (improvement is smaller than anticipated)

--- Year2024/Day10.hs
+++ Year2024/Day10.hs
@@ -4,14 +4,14 @@ import Data.List (nub)
 import Geometry (Grid, Pos, getGridElementSafe, neighbors, zipPoints)
 
 part1, part2 :: Grid Char -> Int
-part1 g = sum . map (length . nub . trails g) . trailHeads $ g
-part2 g = sum . map (length . trails g) . trailHeads $ g
+part1 g = sum . map (trailScore g nub) . trailHeads $ g
+part2 g = sum . map (trailScore g id) . trailHeads $ g
 
 trailHeads :: Grid Char -> [Pos]
 trailHeads = map fst . filter ((== '0') . snd) . zipPoints
 
-trails :: Grid Char -> Pos -> [Pos]
-trails g start = go (9 :: Int)
+trailScore :: Grid Char -> ([Pos] -> [Pos]) -> Pos -> Int
+trailScore g nubfunc start = length $ go (9 :: Int)
   where
     go 0 = [start]
-    go n = concatMap (filter ((== Just ((head . show) n)) . getGridElementSafe g) . neighbors) (go (n - 1))
+    go n = nubfunc $ concatMap (filter ((== Just ((head . show) n)) . getGridElementSafe g) . neighbors) (go (n - 1))
