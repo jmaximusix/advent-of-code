22e4494e644b3ca02a6f86ccb804d13a956606d0
jmaximusix <jmaximusix@gmail.com>
2024-12-11 14:35:10 +0100

    day 11 clean

--- Year2024/Day11.hs
+++ Year2024/Day11.hs
@@ -2,54 +2,25 @@
 
 module Day11 (part1, part2) where
 
-import qualified Data.Foldable as Set
-import Data.IntMap (keysSet)
-import qualified Data.Map as Map (Map, empty, fold, foldWithKey, foldlWithKey, foldrWithKey, fromList, insertWith, keys, keysSet, map, mapKeys, mapWithKey, toList, (!))
-import Debug.Trace (traceShow, traceShowId)
+import qualified Data.Map as Map (empty, foldlWithKey, fromList, insertWith, toList)
+import GHC.Num (integerLogBase)
 
-type Stone = Integer
+part1, part2 :: [String] -> Int
+part1 = solve 25
+part2 = solve 75
 
--- stone, count
-type StoneMap = Map.Map Integer Integer
-
-part1, part2 :: [String] -> Integer
--- part1 = length . repeatNTimes 25 . map read . words . head
-part1 = sum . map snd . Map.toList . applyNTimes 25 . Map.fromList . map ((,1) . read) . words . head
-part2 = sum . map snd . Map.toList . applyNTimes 75 . Map.fromList . map ((,1 :: Integer) . read) . words . head
-
-repeatNTimes :: Int -> [Stone] -> [Stone]
-repeatNTimes 0 stones = stones
-repeatNTimes n stones = concatMap applyRule $ repeatNTimes (n - 1) stones
-
-applyNTimes :: Int -> StoneMap -> StoneMap
-applyNTimes 0 stones = stones
-applyNTimes n stones = soos
+solve :: Int -> [String] -> Int
+solve n = sum . map snd . Map.toList . blink n . Map.fromList . map ((,1) . read) . words . head
   where
-    soos = Map.foldlWithKey nextGen Map.empty $ applyNTimes (n - 1) stones
+    blink 0 stones = stones
+    blink n' stones = Map.foldlWithKey update Map.empty . blink (n' - 1) $ stones
+    update stonemap s c = foldl ((flip . uncurry . Map.insertWith) (+)) stonemap . map (,c) . applyRule $ s
 
-nextGen :: StoneMap -> Stone -> Integer -> StoneMap
-nextGen stones stone count = Map.foldlWithKey insert stones $ Map.fromList $ combine $ map (,count) $ applyRule stone
-
-insert :: StoneMap -> Stone -> Integer -> StoneMap
-insert stones stone count = Map.insertWith (+) stone count stones
-
-combine :: [(Stone, Integer)] -> [(Stone, Integer)]
-combine [] = []
-combine [x] = [x]
-combine [(x, cx), (y, cy)]
-  | x == y = [(x, cx + cy)]
-  | otherwise = [(x, cx), (y, cy)]
-
-applyRule :: Stone -> [Stone]
+applyRule :: Int -> [Int]
 applyRule 0 = [1]
 applyRule n
-  | even (length asstr) = [read before, read after]
+  | even len = [n `div` l', n `rem` l']
   | otherwise = [n * 2024]
   where
-    asstr = show n
-    (before, after) = splitAt (length asstr `div` 2) asstr
-
--- (9456,2),(9494,1),(9888,1),(16192,1),(24579456,1),(32772608,3),(36869184,1)
-
--- (88,1),(94,1),(98,1),(2457,1),
--- (2608,3),(3277,3),(3686,1),(9184,1),(9456,1),(32772608,1)
+    len = integerLogBase 10 (toInteger n) + 1
+    l' = 10 ^ (len `div` 2)
