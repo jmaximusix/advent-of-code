9d75e7e4958f513ab01b87ea5d6bfecf9af909f9
jmaximusix <jmaximusix@gmail.com>
2024-12-11 10:56:39 +0100

    kaputt

--- Year2022/Day16.hs
+++ Year2022/Day16.hs
@@ -1,15 +1,19 @@
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wno-incomplete-record-updates #-}
 
 module Day16 (part1, part2) where
 
 import Algorithm.Search (bfs, dijkstra)
-import Data.List (delete, sort)
+import Control.Applicative (liftA2)
+import Control.Monad.Extra (guarded)
+import Data.List (delete, sort, sortOn)
 import Data.List.Extra (maximumOn, splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map (fromList, (!))
-import Data.Maybe (fromJust, mapMaybe)
+import Data.Maybe (catMaybes, fromJust, fromMaybe, mapMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set (delete, empty, fromList, map, toList)
+import Debug.Trace (traceShow)
 
 type Valve = (Int, Int)
 
@@ -28,32 +32,36 @@ data State = State
   deriving (Show, Eq, Ord)
 
 part1, part2 :: [String] -> Int
-part1 input = pressure $ last path
-  where
-    Just (_, path) = dijkstra (next dmap) (cost dmap) (null . entities) (State [(start, 30)] valves 0)
-    (dmap, valves, start) = parseValves input
-part2 input = pressure $ last path
-  where
-    Just (_, path) = dijkstra (next dmap) (cost dmap) (null . entities) (State [(start, 26), (start, 26)] valves 0)
-    (dmap, valves, start) = parseValves input
+part1 = solve [30] . parseValves
+part2 = solve [26, 26] . parseValves
+
+solve :: [Int] -> (DistanceMap, Set Valve, Int) -> Int
+solve ts' (ds, vs, start) =
+  pressure
+    . last
+    . snd
+    . fromJust
+    $ dijkstra (next ds) (cost ds) (null . entities) (State (map (start,) ts') vs 0)
 
 next :: DistanceMap -> State -> [State]
-next adj state
-  | null soos = [State [] Set.empty (pressure state)]
-  | otherwise = soos
-  where
-    soos = map (maximumOn pressure . moveTo adj state) . Set.toList . remaining $ state
+next adj state =
+  fromMaybe [State [] Set.empty (pressure state)]
+    . guarded (not . null)
+    . mapMaybe (goAndOpenValve adj state)
+    . Set.toList
+    . remaining
+    $ state
 
-moveTo :: DistanceMap -> State -> Valve -> [State]
-moveTo adj (State es r p) v@(vl, f)
-  | null entityandpressure = [State [] r p]
-  | otherwise = [State (sort entities') r' (p + f') | (entities', f') <- entityandpressure]
-  where
-    r' = v `Set.delete` r
-    entityandpressure = mapMaybe (\e -> (\dt -> ((vl, dt) : delete e es, f * dt)) <$> timeAfterReaching adj vl e) es
+goAndOpenValve :: DistanceMap -> State -> Valve -> Maybe State
+goAndOpenValve adj (State es r p) (vl, f) =
+  fmap (\(e', t) -> State (sortOn (negate . snd) ((vl, t) : delete e' es)) r (p + t * f))
+    . guarded (not . null)
+    . maximumOn snd
+    . mapMaybe (liftA2 fmap (,) (maybeTimeAfterReaching adj vl))
+    $ es
 
 maxAchievablePressure :: DistanceMap -> State -> Int
-maxAchievablePressure adj (State es r p) = p + sum (Set.map (\(v, f) -> safemax (mapMaybe (fmap (* f) . timeAfterReaching adj v) es)) r)
+maxAchievablePressure adj (State es r p) = p + sum (Set.map (\(v, f) -> safemax (mapMaybe (fmap (* f) . maybeTimeAfterReaching adj v) es)) r)
   where
     safemax xs
       | null xs = 0
@@ -62,15 +70,12 @@ maxAchievablePressure adj (State es r p) = p + sum (Set.map (\(v, f) -> safemax 
 cost :: DistanceMap -> State -> State -> Cost
 cost adj s1 s2 = maxAchievablePressure adj s1 - maxAchievablePressure adj s2
 
-timeAfterReaching :: DistanceMap -> Int -> Entity -> Maybe Int
-timeAfterReaching adj l' (l, t)
-  | dt > 0 = Just dt
+maybeTimeAfterReaching :: DistanceMap -> Int -> Entity -> Maybe Int
+maybeTimeAfterReaching adj l' (l, t)
+  | t' > 0 = Just t'
   | otherwise = Nothing
   where
-    dt = t - dist adj l l' - 1
-
-dist :: DistanceMap -> Int -> Int -> Int
-dist adj l l' = adj Map.! Set.fromList [l, l']
+    t' = t - (adj Map.! Set.fromList [l, l']) - 1
 
 parseValves :: [String] -> (DistanceMap, Set Valve, Int)
 parseValves lines' = (Map.fromList distmap, Set.fromList valves, start)
