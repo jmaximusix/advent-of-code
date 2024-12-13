68e85f9eb6c5a0acf9859e71728c7372822dbbd9
jmaximusix <jmaximusix@gmail.com>
2024-12-13 16:54:05 +0100

    use gridv

--- Year2024/Day10.hs
+++ Year2024/Day10.hs
@@ -2,17 +2,18 @@ module Day10 (part1, part2) where
 
 import Control.Applicative (liftA2)
 import Data.List (nub)
-import MyLib.Geometry (Grid, Pos, getGridElementSafe, neighbors, zipPoints)
+import qualified Data.Map.Strict as Map
+import MyLib.GridV (GridMap, VecPos, asGridMap, neighbors)
 
-part1, part2 :: Grid Char -> Int
-part1 = sum . liftA2 map (trailScore nub) trailHeads
-part2 = sum . liftA2 map (trailScore id) trailHeads
+part1, part2 :: [[Char]] -> Int
+part1 = sum . liftA2 map (trailScore nub) trailHeads . asGridMap
+part2 = sum . liftA2 map (trailScore id) trailHeads . asGridMap
 
-trailHeads :: Grid Char -> [Pos]
-trailHeads = map fst . filter ((== '0') . snd) . zipPoints
+trailHeads :: GridMap Char -> [VecPos]
+trailHeads = Map.keys . Map.filter (== '0')
 
-trailScore :: ([Pos] -> [Pos]) -> Grid Char -> Pos -> Int
+trailScore :: ([VecPos] -> [VecPos]) -> GridMap Char -> VecPos -> Int
 trailScore nubfunc g start = length (go '9')
   where
     go '0' = [start]
-    go n = nubfunc $ concatMap (filter ((== Just n) . getGridElementSafe g) . neighbors) (go (pred n))
+    go n = nubfunc $ concatMap (filter ((== Just n) . flip Map.lookup g) . neighbors) (go (pred n))
