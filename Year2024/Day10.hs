353ae8331ff8630cb644466abab5e10d52d06763
jmaximusix <jmaximusix@gmail.com>
2024-12-13 16:37:48 +0100

    minor improvements

--- Year2024/Day10.hs
+++ Year2024/Day10.hs
@@ -1,17 +1,18 @@
 module Day10 (part1, part2) where
 
+import Control.Applicative (liftA2)
 import Data.List (nub)
 import MyLib.Geometry (Grid, Pos, getGridElementSafe, neighbors, zipPoints)
 
 part1, part2 :: Grid Char -> Int
-part1 g = sum . map (trailScore g nub) . trailHeads $ g
-part2 g = sum . map (trailScore g id) . trailHeads $ g
+part1 = sum . liftA2 map (trailScore nub) trailHeads
+part2 = sum . liftA2 map (trailScore id) trailHeads
 
 trailHeads :: Grid Char -> [Pos]
 trailHeads = map fst . filter ((== '0') . snd) . zipPoints
 
-trailScore :: Grid Char -> ([Pos] -> [Pos]) -> Pos -> Int
-trailScore g nubfunc start = length $ go (9 :: Int)
+trailScore :: ([Pos] -> [Pos]) -> Grid Char -> Pos -> Int
+trailScore nubfunc g start = length (go '9')
   where
-    go 0 = [start]
-    go n = nubfunc $ concatMap (filter ((== Just ((head . show) n)) . getGridElementSafe g) . neighbors) (go (n - 1))
+    go '0' = [start]
+    go n = nubfunc $ concatMap (filter ((== Just n) . getGridElementSafe g) . neighbors) (go (pred n))
