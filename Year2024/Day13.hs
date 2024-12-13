6e289afec4a921fe6475a1d41d3b3f77185af10f
jmaximusix <jmaximusix@gmail.com>
2024-12-13 07:02:53 +0100

    mathe

--- Year2024/Day13.hs
+++ Year2024/Day13.hs
@@ -1,28 +1,36 @@
 module Day13 (part1, part2) where
 
-import Algorithm.Search (dijkstraAssoc)
 import Data.List.Extra (chunksOf, splitOn)
-import qualified Data.Map.Strict as Map
 import Data.Maybe (mapMaybe)
-import Debug.Trace (traceShow)
+import Data.Tuple.Extra (third3)
 import Linear.V2 (V2 (V2))
 
 part1, part2 :: [String] -> Int
-part1 input = sum $ map fst $ mapMaybe (\(a, b, target) -> dijkstraAssoc (move (a, b)) (\(p, _) -> p == target) (V2 0 0, (0, 0))) $ parseInput input
---   where
---     (a, b, target) = head $ parseInput input
-part2 = undefined
+part1 = sum . mapMaybe solve . parseInput
+part2 = sum . mapMaybe (solve . third3 (fmap (+ 10000000000000))) . parseInput
 
-move :: (V2 Int, V2 Int) -> (V2 Int, (Int, Int)) -> [((V2 Int, (Int, Int)), Int)]
-move (a, b) (pos, (pa, pb))
-  | pa >= 100 || pb > 100 = []
-  | otherwise = [((pos + a, (pa + 1, pb)), 3), ((pos + b, (pa, pb + 1)), 1)]
+solve :: (V2 Int, V2 Int, V2 Int) -> Maybe Int
+solve (V2 ax ay, V2 bx by, V2 tx ty)
+  | enumerator `mod` denominator /= 0 = Nothing
+  | aenum `mod` adenom /= 0 = Nothing
+  | otherwise = Just $ b + 3 * aenum `div` adenom
+  where
+    enumerator = tx * ay - ty * ax
+    denominator = bx * ay - ax * by
+    b = enumerator `div` denominator
+    aenum = tx - b * bx
+    adenom = ax
+
+saveDiv :: Int -> Int -> Maybe Int
+saveDiv a b
+  | a `mod` b == 0 = Just $ a `div` b
+  | otherwise = Nothing
 
 parseInput :: [String] -> [(V2 Int, V2 Int, V2 Int)]
-parseInput lines = map parseMachine machines
+parseInput lines' = map parseMachine machines
   where
     parseMachine [a, b, c] = (parseButton a 2 '+', parseButton b 2 '+', parseButton c 1 '=')
     parseButton s n delim = V2 (read $ init x') (read y')
       where
         [x', y'] = map ((!! 1) . splitOn [delim]) $ drop n $ words s
-    machines = map (take 3) $ chunksOf 4 lines
+    machines = map (take 3) $ chunksOf 4 lines'
