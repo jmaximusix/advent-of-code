7018eab4f43f3878b2293291c0eb1eb5698ba782
jmaximusix <jmaximusix@gmail.com>
2024-12-18 07:08:12 +0100

    use bfs since dijkstra has no advantage here

--- Year2024/Day18.hs
+++ Year2024/Day18.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day18 (part1, part2) where
 
-import Algorithm.Search (dijkstraAssoc)
+import Algorithm.Search (bfs)
 import Data.Maybe (fromJust, isNothing)
 import qualified Data.Set as Set
 import Linear (V2 (..))
@@ -15,13 +13,13 @@ part1 = fromJust . solveUntilN 1024 . blocks
 part2 :: [String] -> String
 part2 input = input !! pred (binarySearch (isNothing . (`solveUntilN` blocks input)) 1025 (length input))
 
-next :: Set.Set VecPos -> VecPos -> [(VecPos, Int)]
-next nblocks = map (,1) . filter (\v'@(V2 a b) -> min a b >= 0 && max a b <= 70 && v' `Set.notMember` nblocks) . neighbors
+next :: Set.Set VecPos -> VecPos -> [VecPos]
+next nblocks = filter (\v'@(V2 a b) -> min a b >= 0 && max a b <= 70 && v' `Set.notMember` nblocks) . neighbors
 
 blocks :: [String] -> [VecPos]
 blocks = map (uncurry V2 . tup2 . readNumbers)
 
 solveUntilN :: Int -> [VecPos] -> Maybe Int
-solveUntilN n allblocks = fst <$> dijkstraAssoc (next nblocks) (== V2 70 70) (V2 0 0)
+solveUntilN n allblocks = length <$> bfs (next nblocks) (== V2 70 70) (V2 0 0)
   where
     nblocks = Set.fromList $ take n allblocks
