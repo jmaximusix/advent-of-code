9d64af54ac53bef4f327bba493736cfbb106c83d
jmaximusix <jmaximusix@gmail.com>
2024-12-19 08:53:11 +0100

    day 19 without shared cache

--- Year2024/Day19.hs
+++ Year2024/Day19.hs
@@ -11,34 +11,32 @@ part1, part2 :: [String] -> Int
 part1 input = length $ filter id $ map (possible patterns) towels
   where
     (patterns, towels) = parseInput input
-part2 input = sum $ map (snd . countWays patterns (Map.singleton "" 1)) towels
+part2 input = sum $ map (countWays patterns) towels
   where
     (patterns, towels) = parseInput input
 
 possible :: Set.Set String -> String -> Bool
 possible patterns s = isJust $ dfs (next patterns) (== "") s
 
-countWays :: Set.Set String -> Map.Map String Int -> String -> (Map.Map String Int, Int)
-countWays patterns cache s = case Map.lookup s cache of
-  Just x -> (cache, x)
-  Nothing -> if cache' == cache then (cache, 0) else countWays patterns cache' s
-    where
-      soos = tail (reverse (tails s))
-      cache' =
-        foldl
-          ( \c t ->
-              foldl
-                ( \c' i ->
-                    if i `Set.member` patterns
-                      then Map.insertWith (+) t (fromMaybe 0 $ Map.lookup (rest i t) c) c'
-                      else Map.insertWith (+) t 0 c'
-                )
-                c
-                (take 8 $ tail $ inits t)
-          )
-          cache
-          soos
-      rest i' = drop (length i')
+countWays :: Set.Set String -> String -> Int
+countWays patterns s = cache' Map.! s
+  where
+    soos = tail (reverse (tails s))
+    cache' =
+      foldl
+        ( \c t ->
+            foldl
+              ( \c' i ->
+                  if i `Set.member` patterns
+                    then Map.insertWith (+) t (fromMaybe 0 $ Map.lookup (rest i t) c) c'
+                    else Map.insertWith (+) t 0 c'
+              )
+              c
+              (take 8 $ tail $ inits t)
+        )
+        (Map.singleton "" 1)
+        soos
+    rest i' = drop (length i')
 
 next :: Set.Set String -> String -> [String]
 next patterns s = map (`drop` s) soos
