6d7ee0f7149624c79febcb405ea3fc0de0f464ac
jmaximusix <jmaximusix@gmail.com>
2024-12-20 09:05:02 +0100

    day 20 cleanup

--- Year2024/Day20.hs
+++ Year2024/Day20.hs
@@ -1,31 +1,29 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day20 (part1, part2) where
 
 import Algorithm.Search (bfs, pruning)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust, mapMaybe)
-import qualified Data.Set as Set
 import Linear (V2 (V2))
-import MyLib.GridV
+import MyLib.GridV (VecPos, asGridMap, neighbors, pullPois)
+import MyLib.Utils (count)
 
 part1, part2 :: [String] -> Int
 part1 = solve 2
 part2 = solve 20
 
 solve :: Int -> [String] -> Int
-solve cheattime input = length cheats
+solve cheattime input = sum . map (countCheats cheattime nocheats) $ Map.keys nocheats
   where
     (grid, [start, end]) = pullPois "SE" '.' $ asGridMap input
+    -- technically we should also map locations that aren't on the shortest path from start to end but apparently it doesn't matter with the inputs
     nocheats = Map.fromList $ zip (end : fromJust (bfs (neighbors `pruning` (\v -> Map.lookup v grid == Just '#')) (== start) end)) [0 ..]
-    cheats = foldl (countCheats cheattime nocheats) Set.empty $ Map.keys nocheats
 
-countCheats :: Int -> Map.Map VecPos Int -> Set.Set (VecPos, VecPos) -> VecPos -> Set.Set (VecPos, VecPos)
-countCheats ct nocheats cheats p = foldl (\c ((v, _), _) -> (p, v) `Set.insert` c) cheats successful
+countCheats :: Int -> Map.Map VecPos Int -> VecPos -> Int
+countCheats ct nocheats p =
+  count
+    (\t' -> (nocheats Map.! p) - t' >= 100)
+    (mapMaybe (\(v, d) -> (+ d) <$> Map.lookup v nocheats) candidates)
   where
-    successful =
-      filter (\((v, d), i) -> (nocheats Map.! p) - i - d >= 100 && (p, v) `Set.notMember` cheats) $
-        mapMaybe (\a@(v, _) -> (a,) <$> Map.lookup v nocheats) candidates
     candidates =
       [ (p + V2 dx dy, abs dx + abs dy)
         | dx <- [-ct .. ct],
