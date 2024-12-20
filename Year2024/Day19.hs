34314a275632ca9fdf7cb0e29eec7084417b9f76
jmaximusix <jmaximusix@gmail.com>
2024-12-20 08:38:20 +0100

    small refactoring

--- Year2024/Day19.hs
+++ Year2024/Day19.hs
@@ -4,44 +4,44 @@ import Algorithm.Search (dfs)
 import Data.List (inits, tails)
 import Data.List.Extra (splitOn)
 import qualified Data.Map as Map
-import Data.Maybe (fromMaybe, isJust)
+import Data.Maybe (isJust)
 import qualified Data.Set as Set
 
 part1, part2 :: [String] -> Int
 part1 input = length $ filter id $ map (possible patterns) towels
   where
     (patterns, towels) = parseInput input
-part2 input = sum $ map (countWays patterns) towels
+part2 input = sum $ map (countWays patterns maxlen) towels
   where
+    maxlen = Set.findMax . Set.map length $ patterns
     (patterns, towels) = parseInput input
 
 possible :: Set.Set String -> String -> Bool
 possible patterns s = isJust $ dfs (next patterns) (== "") s
 
-countWays :: Set.Set String -> String -> Int
-countWays patterns s = cache' Map.! s
+countWays :: Set.Set String -> Int -> String -> Int
+countWays patterns maxlen s = cache Map.! s
   where
-    soos = tail (reverse (tails s))
-    cache' =
+    cache =
       foldl
         ( \c t ->
             foldl
               ( \c' i ->
                   if i `Set.member` patterns
-                    then Map.insertWith (+) t (fromMaybe 0 $ Map.lookup (rest i t) c) c'
+                    then Map.insertWith (+) t (c Map.! drop (length i) t) c'
                     else Map.insertWith (+) t 0 c'
               )
               c
-              (take 8 $ tail $ inits t)
+              (take maxlen $ tail $ inits t)
         )
         (Map.singleton "" 1)
-        soos
-    rest i' = drop (length i')
+        . tail
+        . reverse
+        . tails
+        $ s
 
 next :: Set.Set String -> String -> [String]
-next patterns s = map (`drop` s) soos
-  where
-    soos = map length $ filter (`Set.member` patterns) $ tail $ inits s
+next patterns s = map ((`drop` s) . length) . filter (`Set.member` patterns) . tail . inits $ s
 
 parseInput :: [String] -> (Set.Set String, [String])
 parseInput input = (Set.fromList patterns', towels)
