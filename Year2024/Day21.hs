cf83a10f9cd2fce57567e50e3087605d382f0617
JMaximusIX <nix@gibts>
2024-12-21 09:42:24 +0100

    bf ( no chance)

--- Year2024/Day21.hs
+++ Year2024/Day21.hs
@@ -1,43 +1,44 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day21 (part1, part2) where
 
-import Algorithm.Search (bfs, dijkstraAssoc, pruning)
+import Algorithm.Search (bfs, dijkstraAssoc, dijkstraAssocM, pruning)
 import Data.Bifunctor (first)
-import Data.Foldable (find)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust, isNothing, mapMaybe)
-import qualified Data.Set as Set
 import Data.Tuple (swap)
-import Debug.Trace (traceShow)
 import Linear (V2 (V2))
 import MyLib.GridV
 import MyLib.Utils
 
 part1, part2 :: [String] -> Int
-part1 input = sum $ map (uncurry (*) . first (solve arrowkeys numpad) . parseInput) input
+part1 input = sum $ map (uncurry (*) . first (solve arrowkeys numpad 2) . parseInput) input
   where
-    state0 = State (V2 2 3) (V2 2 0) (V2 2 0)
     numpad = Map.fromList $ map swap [('7', V2 0 0), ('8', V2 1 0), ('9', V2 2 0), ('4', V2 0 1), ('5', V2 1 1), ('6', V2 2 1), ('1', V2 0 2), ('2', V2 1 2), ('3', V2 2 2), ('0', V2 1 3), ('A', V2 2 3)]
     arrowkeys = Map.fromList $ map swap [('^', V2 1 0), ('A', V2 2 0), ('<', V2 0 1), ('v', V2 1 1), ('>', V2 2 1)]
-part2 input = undefined
+part2 input = sum $ map (uncurry (*) . first (solve arrowkeys numpad 25) . parseInput) input
+  where
+    numpad = Map.fromList $ map swap [('7', V2 0 0), ('8', V2 1 0), ('9', V2 2 0), ('4', V2 0 1), ('5', V2 1 1), ('6', V2 2 1), ('1', V2 0 2), ('2', V2 1 2), ('3', V2 2 2), ('0', V2 1 3), ('A', V2 2 3)]
+    arrowkeys = Map.fromList $ map swap [('^', V2 1 0), ('A', V2 2 0), ('<', V2 0 1), ('v', V2 1 1), ('>', V2 2 1)]
 
-solveC :: Map.Map VecPos Char -> Map.Map VecPos Char -> Char -> Char -> Int
-solveC apad numpad r1c c = length $ fromJust $ bfs (next apad numpad `pruning` wrongAnswer c) (rightAnswer c) state0
+solveC :: Map.Map VecPos Char -> Map.Map VecPos Char -> Int -> Char -> Char -> Int
+solveC apad numpad n r1c c = fst $ fromJust $ dijkstraAssoc (filter (not . wrongAnswer c . fst) . next apad numpad) (rightAnswer c) state0
   where
     r1loc = fst $ Map.findMin $ Map.filter (== r1c) numpad
-    state0 = State r1loc (V2 2 0) (V2 2 0)
+    state0 = State r1loc 0 $ replicate n $ V2 2 0
 
 parseInput :: String -> (String, Int)
 parseInput s = ('A' : s, read $ init s)
 
-solve :: Map.Map VecPos Char -> Map.Map VecPos Char -> String -> Int
-solve apad numpad [a, b] = solveC apad numpad a b
-solve apad numpad (a : b : rest) = solveC apad numpad a b + solve apad numpad (b : rest)
+solve :: Map.Map VecPos Char -> Map.Map VecPos Char -> Int -> String -> Int
+solve apad numpad n [a, b] = solveC apad numpad n a b
+solve apad numpad n (a : b : rest) = solveC apad numpad n a b + solve apad numpad n (b : rest)
 
 data State
   = State
       { robot1 :: VecPos,
-        robot2 :: VecPos,
-        robot3 :: VecPos
+        index :: Int,
+        betweens :: [VecPos]
       }
   | Output Char
   deriving (Show, Eq, Ord)
@@ -50,34 +51,32 @@ rightAnswer :: Char -> State -> Bool
 rightAnswer c (Output o) = c == o
 rightAnswer _ _ = False
 
-next :: Map.Map VecPos Char -> Map.Map VecPos Char -> State -> [State]
-next apad numpad state = mapMaybe (simulateInput apad numpad state) "^v<>A"
+next :: Map.Map VecPos Char -> Map.Map VecPos Char -> State -> [(State, Int)]
+next apad numpad state = map (,1) $ mapMaybe (simulateInput apad numpad state) "^v<>A"
 
 simulateInput :: Map.Map VecPos Char -> Map.Map VecPos Char -> State -> Char -> Maybe State
-simulateInput apad numpad state@(State r1 r2 r3) c
-  | c == 'A' = press3 apad numpad state
-  | isNothing mr3' = Nothing
-  | otherwise = Just $ State r1 r2 (r3 + toDir c)
+simulateInput apad numpad state@(State r1 i rs) c
+  | c == 'A' = pressbetween apad numpad state {index = i + 1}
+  | isNothing mr' = Nothing
+  | otherwise = Just $ State r1 0 (replace i (r + toDir c) rs)
   where
-    mr3' = Map.lookup (r3 + toDir c) apad
+    mr' = Map.lookup (r + toDir c) apad
+    r = rs !! i
 
-press3 :: Map.Map VecPos Char -> Map.Map VecPos Char -> State -> Maybe State
-press3 apad numpad state@(State r1 r2 r3)
-  | c == 'A' = press2 apad numpad state
-  | isNothing mr2' = Nothing
-  | otherwise = Just $ State r1 (r2 + toDir c) r3
+pressbetween :: Map.Map VecPos Char -> Map.Map VecPos Char -> State -> Maybe State
+-- pressbetween _ _ (State _ i rs) | traceShow (i, rs) False = undefined
+pressbetween apad numpad state@(State r1 i rs)
+  | i == length rs && c == 'A' = Just $ Output (numpad Map.! r1)
+  | i == length rs && isNothing mr1' = Nothing
+  | i == length rs = Just $ State (r1 + toDir c) 0 rs
+  | c == 'A' = pressbetween apad numpad state {index = i + 1}
+  | isNothing mr' = Nothing
+  | otherwise = Just $ State r1 0 (replace i (r + toDir c) rs)
   where
-    mr2' = Map.lookup (r2 + toDir c) apad
-    c = apad Map.! r3
-
-press2 :: Map.Map VecPos Char -> Map.Map VecPos Char -> State -> Maybe State
-press2 apad numpad state@(State r1 r2 r3)
-  | c == 'A' = Just $ Output (numpad Map.! r1)
-  | isNothing mr1' = Nothing
-  | otherwise = Just $ State (r1 + toDir c) r2 r3
-  where
+    mr' = Map.lookup (r + toDir c) apad
     mr1' = Map.lookup (r1 + toDir c) numpad
-    c = apad Map.! r2
+    r = rs !! i
+    c = apad Map.! (rs !! (i - 1))
 
 toDir :: Char -> Direction
 toDir '^' = dir N
@@ -103,3 +102,8 @@ toDir '>' = dir E
 -- translateArrowMoveToArrow pad p1 p2
 
 -- transitionCostOnInner
+
+-- A A A A A A A A X
+-- moving up 1
+-- A A A A A A A U X
+-- A A A A A A L
