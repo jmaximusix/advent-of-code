ef39c92356bdb4d4fe792946425a76beebe60b80
JMaximusIX <nix@gibts>
2024-12-22 16:20:27 +0100

    Day 21 submit

--- Year2024/Day21.hs
+++ Year2024/Day21.hs
@@ -4,7 +4,7 @@ module Day21 (part1, part2) where
 
 import Algorithm.Search (bfs, dijkstraAssoc, dijkstraAssocM, pruning)
 import Combinatorics (variate)
-import Data.Bifunctor (first)
+import Data.Bifunctor (Bifunctor (bimap), first)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust, isNothing, mapMaybe)
 import Data.Tuple (swap)
@@ -18,9 +18,10 @@ part1 input = sum $ map (uncurry (*) . first (solve arrowkeys numpad 2) . parseI
   where
     numpad = Map.fromList $ map swap [('7', V2 0 0), ('8', V2 1 0), ('9', V2 2 0), ('4', V2 0 1), ('5', V2 1 1), ('6', V2 2 1), ('1', V2 0 2), ('2', V2 1 2), ('3', V2 2 2), ('0', V2 1 3), ('A', V2 2 3)]
     arrowkeys = Map.fromList $ map swap [('^', V2 1 0), ('A', V2 2 0), ('<', V2 0 1), ('v', V2 1 1), ('>', V2 2 1)]
-part2 input = sum solution
+part2 input = sum solution2
   where
-    solution = map (uncurry (*) . first (seqLen . robotifySeq numpad 25) . parseInput) input
+    solution2 = map (uncurry (*) . first (fst . robotifySeq2 numpad 100000) . parseInput) input
+    solution = map (uncurry (*) . first (seqLen . robotifySeq numpad 2) . parseInput) input
     keylist = [('7', V2 0 0), ('8', V2 1 0), ('9', V2 2 0), ('4', V2 0 1), ('5', V2 1 1), ('6', V2 2 1), ('1', V2 0 2), ('2', V2 1 2), ('3', V2 2 2), ('0', V2 1 3), ('A', V2 2 3)]
     troubleshoot = filter (\s -> length s /= 6) $ zipWith (\(a, b) d -> if b == d then a ++ " top" else a ++ " passt nicht: " ++ show b ++ ", " ++ show d) correct incorrect
     correct = map (\x -> (x, solve arrowkeys numpad' 5 x)) $ variate 2 "0123456789A"
@@ -28,12 +29,6 @@ part2 input = sum solution
     numpad' = Map.fromList $ map swap keylist
     numpad = Map.fromList keylist
     arrowkeys = Map.fromList $ map swap [('^', V2 1 0), ('A', V2 2 0), ('<', V2 0 1), ('v', V2 1 1), ('>', V2 2 1)]
-
--- [25,12,19,26,13,20,27,14,21,10,21,10,11,12,19,20,13,20,21,22,16,18,10,21,12,19,22,13,20,21,21,19,18,22,21,12,23,22,13,16,22,16,21,22,10,11,12,19,20,23,17,21,16,21,18,10,21,12,19,22,22,22,21,16,19,18,22,21,12,17,23,17,22,23,16,21,22,10,11,24,18,22,17,22,21,16,21,18,10,23,23,23,22,17,22,21,16,19,18,18,18,26,21,12,27,22,13,28,23,14]
--- [25,12,19,26,13,20,27,14,21,10,21,10,11,12,19,20,13,20,21,22,16,18,10,21,12,19,22,13,20,17,21,19,18,22,21,12,23,22,13,16,22,16,17,18,10,11,12,19,20,23,17,21,16,17,18,10,21,12,19,18,22,22,21,16,19,18,22,21,12,17,23,17,18,19,16,17,18,10,11,24,18,22,17,18,21,16,17,18,10,19,23,23,22,17,22,21,16,19,18,18,18,26,21,12,27,22,13,28,23,14]
--- part2 input = sum $ map (uncurry (*) . first (seqLen . robotifySeq numpad 2) . parseInput) input
---   where
---     numpad = Map.fromList [('7', V2 0 0), ('8', V2 1 0), ('9', V2 2 0), ('4', V2 0 1), ('5', V2 1 1), ('6', V2 2 1), ('1', V2 0 2), ('2', V2 1 2), ('3', V2 2 2), ('0', V2 1 3), ('A', V2 2 3)]
 
 solveC :: Map.Map VecPos Char -> Map.Map VecPos Char -> Int -> Char -> Char -> Int
 solveC apad numpad n r1c c = fst $ fromJust $ dijkstraAssoc (filter (not . wrongAnswer c . fst) . next apad numpad) (rightAnswer c) state0
@@ -101,6 +96,7 @@ simulateInput apad numpad state@(State r1 i rs) c
     mr' = Map.lookup (r + toDir c) apad
     r = rs !! i
 
+mapad :: Sequence -> Sequence
 mapad p@(Press _) = p
 mapad (Nt a 'v') = Seq [Seq [Nt 1 '<', Nt 1 'v'], Press a, Seq [Nt 1 '^', Nt 1 '>']]
 mapad (Nt a '^') = Seq [Nt 1 '<', Press a, Nt 1 '>']
@@ -115,6 +111,42 @@ mapad (Seq [Nt a '<', Nt b '^']) = Seq [Seq [Nt 1 'v', Nt 2 '<'], Press a, Seq [
 mapad (Seq [Nt a '^', Nt b '<']) = Seq [Nt 1 '<', Press a, Seq [Nt 1 'v', Nt 1 '<'], Press b, Seq [Nt 2 '>', Nt 1 '^']]
 mapad (Seq [Nt a 'v', Nt b '<']) = Seq [Seq [Nt 1 '<', Nt 1 'v'], Press a, Nt 1 '<', Press b, Seq [Nt 2 '>', Nt 1 '^']]
 mapad (Seq xs) = Seq $ map mapad xs
+
+mapad2 :: String -> (Int, [String])
+mapad2 "v" = (4, ["<v", "^>"])
+mapad2 "^" = (2, ["<", ">"])
+mapad2 ">" = (2, ["v", "^"])
+mapad2 "<" = (6, ["v<", ">^"])
+mapad2 "v>" = (4, ["<v", ">", "^"])
+mapad2 ">v" = (4, ["v", "<", "^>"])
+mapad2 ">^" = (4, ["v", "<^", ">"])
+mapad2 "^>" = (4, ["<", "v>", "^"])
+mapad2 "<v" = (6, ["v<", ">", "^>"])
+mapad2 "<^" = (6, ["v<", ">^", ">"])
+mapad2 "^<" = (6, ["<", "v<", ">^"])
+mapad2 "v<" = (6, ["<v", "<", ">^"])
+
+type Sequence2 = (Int, Map.Map [Char] Int)
+
+moves2 :: VecPos -> VecPos -> (Int, String)
+moves2 v1@(V2 x1 y1) v2@(V2 x2 y2)
+  | x2 == 0 && y1 == 3 = (d, "^<")
+  | x1 == 0 && y2 == 3 = (d, ">v")
+  | otherwise = case sv of
+      V2 0 1 -> (dy, "v")
+      V2 0 (-1) -> (dy, "^")
+      V2 1 0 -> (dx, ">")
+      V2 (-1) 0 -> (dx, "<")
+      V2 1 1 -> (d, "v>")
+      V2 1 (-1) -> (d, "^>")
+      V2 (-1) 1 -> (d, "<v")
+      V2 (-1) (-1) -> (d, "<^")
+  where
+    (V2 x y) = v2 - v1
+    sv = V2 (signum x) (signum y)
+    dx = abs x
+    dy = abs y
+    d = dx + dy
 
 moves :: VecPos -> VecPos -> Sequence
 moves v1@(V2 x1 y1) v2@(V2 x2 y2)
@@ -134,6 +166,21 @@ moves v1@(V2 x1 y1) v2@(V2 x2 y2)
     dv = V2 (signum x) (signum y)
     ax = abs x
     ay = abs y
+
+robotifySeq2 :: Map.Map Char VecPos -> Int -> [Char] -> Sequence2
+robotifySeq2 numpad 0 [a, b] = bimap (+ 1) (`Map.singleton` 1) (moves2 (numpad Map.! a) (numpad Map.! b))
+robotifySeq2 numpad 0 (a : b : rest) = (\[(a', b'), (c, d)] -> (a' + c, Map.unionWith (+) b' d)) bots
+  where
+    bots = [robotifySeq2 numpad 0 [a, b], robotifySeq2 numpad 0 (b : rest)]
+robotifySeq2 np n cs = (presses, moves')
+  where
+    -- (c, d) = bimap ((+ a) . sum) (countAll . concat) $ unzip $ map mapad2 b
+    (presses, moves') = Map.foldlWithKey update (a, Map.empty) b
+    update (acc, ms) move c = (acc + c * dacc, foldl (update2 c) ms newmoves)
+      where
+        (dacc, newmoves) = mapad2 move
+    update2 c ms newm = Map.insertWith (+) newm c ms
+    (a, b) = robotifySeq2 np (n - 1) cs
 
 robotifySeq :: Map.Map Char VecPos -> Int -> [Char] -> Sequence
 robotifySeq numpad 0 [a, b] = Seq [moves (numpad Map.! a) (numpad Map.! b), Press 1]
