9f01e5e6f638ef6a32b3c4d695e136870c9e4e8f
JMaximusIX <nix@gibts>
2024-12-23 09:31:17 +0100

    Day23 clean

--- Year2024/Day23.hs
+++ Year2024/Day23.hs
@@ -1,84 +1,46 @@
 module Day23 (part1, part2) where
 
-import Data.List (intercalate, nub)
+import Data.List (find, intercalate)
 import qualified Data.Map.Strict as Map
+import Data.Maybe (fromJust, isNothing)
+import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Tuple.Extra (both, dupe, second)
-import Debug.Trace (traceShow, traceShowId)
-import MyLib (count, countAll, tup2)
+import Data.Tuple.Extra (second)
 
-part1, part2 :: [String] -> Int
-part1 input = traceShow (parties2) sol
+type Connections = Map.Map String (Set String)
+
+part1 :: [String] -> Int
+part1 = Set.size . allPartiesWithChief . parseInput
+
+part2 :: [String] -> String
+part2 = intercalate "," . Set.toAscList . largestParty . parseInput
+
+parseInput :: [String] -> Connections
+parseInput = foldl ins Map.empty . map (second tail . splitAt 2)
   where
-    sol = count (any ((== 't') . head) . Set.toList) $ Set.toList parties2
-    diffa = Set.difference parties1 parties2
-    diffb = Set.difference parties2 parties1
-    parties2 = allLanPartiesOfSize 13 conns Set.empty
-    parties1 = Set.fromList $ allLanParties conns []
-    estmax = maximum $ Map.unionWith (+) estmaxl estmaxr
-    estmaxl = countAll $ map fst conns
-    estmaxr = countAll $ map snd conns
-    conns = map parseInput input
-part2 = undefined
+    ins m (a, b) = ins' a b (ins' b a m)
+    ins' a b = Map.insertWith Set.union a (Set.singleton b)
 
-password :: Set.Set String -> String
-password = intercalate "," . Set.toAscList
+allPartiesWithChief :: Connections -> Set (Set String)
+allPartiesWithChief conns = Set.unions . map (partiesWithMember 3 conns) . filter ((== 't') . head) . Map.keys $ conns
 
-parseInput :: String -> (String, String)
-parseInput = second tail . splitAt 2
-
-partners :: [(String, String)] -> String -> [String]
-partners conns target = left ++ right
+largestParty :: Connections -> Set String
+largestParty conns
+  | isNothing maybeparties = largestParty rest
+  | otherwise = Set.findMin $ fromJust maybeparties
   where
-    left = map snd $ filter ((== target) . fst) conns
-    right = map fst $ filter ((== target) . snd) conns
+    maybeparties = find (not . null) $ map (partiesWithMember maxsize conns) $ Map.keys longest
+    (longest, rest) = Map.partition ((== maxsize) . length) conns
+    maxsize = maximum $ Map.map length conns
 
-allLanParties :: [(String, String)] -> [Set.Set String] -> [Set.Set String]
-allLanParties [] acc = acc
-allLanParties conns acc = allLanParties remaining (party ++ acc)
+partiesWithMember :: Int -> Connections -> String -> Set (Set String)
+partiesWithMember 2 conns p = Set.map (\a -> Set.fromList [p, a]) $ conns Map.! p
+partiesWithMember n conns p = Set.unions $ Set.map tryAddNeighbors $ partiesWithMember (n - 1) conns p
   where
-    remaining = filter (\(a, b) -> not (a == c0 || b == c0)) conns
-    party = findLanParty conns c0
-    c0 = fst $ head conns
+    tryAddNeighbors party = Set.unions $ Set.map (\m -> tryAdd party m conns) party
 
-findLanParty :: [(String, String)] -> String -> [Set.Set String]
-findLanParty conns p = lans
+tryAdd :: Set String -> String -> Connections -> Set (Set String)
+tryAdd party pmember conns = Set.map (`Set.insert` party) successful
   where
-    lans = nub $ [Set.fromList [p, a, b] | (a, bs) <- soos, b <- bs]
-    soos = zip next $ map (filter (`elem` next) . partners conns) next
-    next = partners conns p
-
-allLanPartiesOfSize :: Int -> [(String, String)] -> Set.Set (Set.Set String) -> Set.Set (Set.Set String)
-allLanPartiesOfSize n conn acc | traceShow (n, length conn, acc) False = undefined
-allLanPartiesOfSize _ [] acc = acc
-allLanPartiesOfSize n conns acc = allLanPartiesOfSize n remaining (acc `Set.union` Set.fromList party)
-  where
-    remaining = filter (\(a, b) -> not (a == c0 || b == c0)) conns
-    party = lanPartiesOfSize n conns c0
-    c0 = fst $ head conns
-
-lanPartiesOfSize :: Int -> [(String, String)] -> String -> [Set.Set String]
-lanPartiesOfSize 2 conns p = [Set.fromList [p, a] | a <- partners conns p]
-lanPartiesOfSize n conns p = this
-  where
-    this = nub $ concatMap (`addOne` conns) onesmaller
-    onesmaller = lanPartiesOfSize (n - 1) conns p
-
-addOne :: Set.Set String -> [(String, String)] -> [Set.Set String]
--- addOne p c | traceShow (p, c) False = undefined
-addOne party conns = soos
-  where
-    soos = nub $ concatMap (\p -> addOne' party p conns) ps
-    ps = Set.toList party
-
-addOne' :: Set.Set String -> String -> [(String, String)] -> [Set.Set String]
-addOne' party pmember conns = ps
-  where
-    ps = map (`Set.insert` party) soos
-    soos = filter (isConnectedToAll party conns) next
-    next = filter (`Set.notMember` party) $ partners conns pmember
-
-isConnectedToAll :: Set.Set String -> [(String, String)] -> String -> Bool
-isConnectedToAll party conns p = all (`elem` ps) party
-  where
-    ps = partners conns p
+    successful = Set.filter (null . Set.difference party . (conns Map.!)) potential
+    potential = Set.difference (conns Map.! pmember) party
