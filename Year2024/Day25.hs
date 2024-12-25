85cd2b085cb439230ef47d91d8dc14700e420503
JMaximusIX <nix@gibts>
2024-12-25 08:41:26 +0100

    Day 25

--- Year2024/Day25.hs
+++ Year2024/Day25.hs
@@ -1,5 +1,22 @@
 module Day25 (part1, part2) where
 
-part1, part2 :: [String] -> Int
-part1 = undefined
-part2 = undefined
+import Data.List (partition, transpose)
+import Data.List.Extra (chunksOf)
+import Data.Tuple.Extra (both)
+import MyLib.Utils (count, countEq)
+
+part1 :: [String] -> Int
+part1 input = count (all (< 8)) [zipWith (+) l k | l <- locks, k <- keys]
+  where
+    (locks, keys) = parseInput input
+
+part2 :: [String] -> String
+part2 _ = "Yay, this is the first AoC I completely finished :D"
+
+parseInput :: [String] -> ([[Int]], [[Int]])
+parseInput =
+  both
+    (map (map (countEq '#') . transpose))
+    . partition (all (== '.') . filter (`elem` ".#") . last)
+    . map (filter (not . null))
+    . chunksOf 8
