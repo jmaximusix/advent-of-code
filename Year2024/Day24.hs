fef8964958ed592281b9b78fb828a18d5edebcb4
JMaximusIX <nix@gibts>
2024-12-26 11:27:25 +0100

    basically working

--- Year2024/Day24.hs
+++ Year2024/Day24.hs
@@ -1,21 +1,23 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE TupleSections #-}
 
 module Day24 (part1, part2) where
 
-import Control.Monad (when)
+import Algorithm.Search (aStar, bfs, dfs)
+import Control.Lens (Bifunctor (bimap))
 import Data.Bits (xor, (.&.), (.|.))
 import Data.List.Extra (splitOn, uncons)
-import qualified Data.Map.Strict as Map
-import Data.Maybe (fromJust)
-import Data.SBV (SBool, SDivisible (sMod), SInt64, Symbolic, ThmResult, free, isTheorem, oneIf, output, prove, runSMT, sBools, sFalse, (.&&), (.<+>), (.<=>), (.==), (.>>.), (.||))
+import qualified Data.Map as Map
+import Data.Maybe (fromJust, isNothing, listToMaybe)
+import Data.SBV (SBool, SInt64, Symbolic, isTheorem, oneIf, sBools, sMod, (.&&), (.<+>), (.<=>), (.==), (.>>.), (.||))
 import qualified Data.Set as Set
-import Data.Tuple.Extra (both)
+import Data.Tuple.Extra (both, thd3)
 import Debug.Trace (traceShow, traceShowId)
-import MyLib (binarySearch, tup2)
+import MyLib (Part (..), binarySearch, tup2)
 import System.IO.Unsafe (unsafePerformIO)
 import Text.Printf (printf)
 
-type EMap = Map.Map String Entry
+type EntryMap = Map.Map String Entry
 
 type Bools = Map.Map String SBool
 
@@ -23,133 +25,156 @@ part1 :: [String] -> Int
 part1 input = result
   where
     result = foldl1 (\a b -> 2 * a + b) $ reverse solved
-    solved = map ((\(Lit l) -> l) . snd) $ Map.toList $ Map.map (solve mappings) zs
-    zs = Map.filterWithKey (\k _ -> head k == 'z') mappings
-    mappings = parseInput input
+    solved = map ((\(LitRoot l) -> l) . snd) $ Map.toList $ Map.map (solve mappings) zs
+    zs = Map.filterWithKey (\k _ -> listToMaybe k == Just 'z') mappings
+    mappings = parseInput Part1 input
 
 asSint :: Bools -> Char -> SInt64
 asSint bs c = foldl (\acc a -> 2 * acc + (oneIf a :: SInt64)) 0 $ reverse bs'
   where
-    bs' = map snd $ Map.toList $ Map.filterWithKey (\k _ -> head k == c) bs
+    bs' = map snd $ Map.toList $ Map.filterWithKey (\k _ -> listToMaybe k == Just c) bs
 
 part2 :: [String] -> String
-part2 input = show (correctUntilN 45 zs)
+part2 input = show $ searchres
   where
-    mappings = parseInput2 input
-    zs' = Map.filterWithKey (\k _ -> head k == 'z') mappings
-    wds = Map.map (wiredeps mappings) zs'
-    zs = traceShow wds $ Map.map (solve3 mappings) zs'
-    mappings3 = Map.map (solve3 mappings) mappings
-    wds' = Map.mapWithKey Set.insert wds
-    ds = Map.map deps mappings3
-    breakingAt = binarySearch (not . flip correctUntilN zs) 0 45
+    symmap = parseInput Part2 input
+    zs = Map.filterWithKey (\k _ -> listToMaybe k == Just 'z') symmap
+    zTrees = Map.map (buildtree symmap) zs
+    searchres = recurse (symmap, Set.empty)
 
-correctUntilN :: Int -> Map.Map String Entry3 -> Bool
+recurse :: (EntryMap, Set.Set String) -> Set.Set String
+recurse (symmap, swaps) | traceShow swaps False = undefined
+recurse (symmap, swaps)
+  | isGoal nexte = snd nexte
+  | otherwise = recurse nexte
+  where
+    nexte = head $ next (symmap, swaps)
+
+isGoal :: (EntryMap, Set.Set String) -> Bool
+isGoal (symmap, s)
+  | Set.size s /= 8 = False
+  | otherwise = isNothing maybeBreakingAt
+  where
+    zs = Map.filterWithKey (\k _ -> listToMaybe k == Just 'z') symmap
+    zTrees = Map.map (buildtree symmap) zs
+    maybeBreakingAt = binarySearch (not . flip correctUntilN zTrees) 0 45
+
+cost :: (EntryMap, Set.Set String, Maybe Int) -> (EntryMap, Set.Set String, Maybe Int) -> Int
+cost (_, _, Just a) (_, _, Just b) = b - a
+cost (_, _, Just a) (_, _, Nothing) = 46 - a
+
+heuristic :: (EntryMap, Set.Set String, Maybe Int) -> Int
+heuristic (_, _, Just a) = 46 - a
+heuristic (_, _, Nothing) = 0
+
+next :: (EntryMap, Set.Set String) -> [(EntryMap, Set.Set String)]
+next (_, swapped) | traceShow swapped False = undefined
+next (symmap, swapped) = newsymmaps
+  where
+    trees = Map.map (buildtree symmap) symmap
+    ds = Map.map deps trees
+    zs = Map.filterWithKey (\k _ -> listToMaybe k == Just 'z') trees
+    (Just br) = binarySearch (not . flip correctUntilN zs) 0 45
+    toSwap = wiredeps $ zs Map.! cStr 'z' br
+    excl1 = wiredeps $ zs Map.! cStr 'z' (br - 1)
+    pot = Map.keysSet $ Map.filter (isRelevant br) ds
+    toSwap' = toSwap `Set.difference` (Set.union swapped excl1)
+    candidates = pot `Set.difference` (Set.unions [swapped, excl1])
+    newsymmaps = traceShow (toSwap', candidates) filter (\(a, b) -> traceShow b emapCorrectUntil br a) $ map (swapInMap symmap swapped) [(a, b) | a <- Set.toList candidates, b <- Set.toList toSwap', swapPossible trees a b]
+
+swapPossible :: Map.Map String TreeEntry -> String -> String -> Bool
+swapPossible trees a b = a /= b && a `Set.notMember` wdb && b `Set.notMember` wda
+  where
+    wda = wiredeps aTree
+    wdb = wiredeps bTree
+    aTree = trees Map.! a
+    bTree = trees Map.! b
+
+emapCorrectUntil :: Int -> EntryMap -> Bool
+emapCorrectUntil n _ | traceShow n False = undefined
+emapCorrectUntil n symmap = correctUntilN n zTrees
+  where
+    zs = Map.filterWithKey (\k _ -> listToMaybe k == Just 'z') symmap
+    zTrees = Map.map (buildtree symmap) zs
+
+swapInMap :: EntryMap -> Set.Set String -> (String, String) -> (EntryMap, Set.Set String)
+swapInMap m swapped (a, b) = (Map.insert a (m Map.! b) $ Map.insert b (m Map.! a) m, Set.union (Set.fromList [a, b]) swapped)
+
+isRelevant :: Int -> Set.Set String -> Bool
+isRelevant n s = (s `Set.isSubsetOf` (Set.fromList (xyStrsUntilN n))) && (Set.fromList [(cStr 'x' n), cStr 'y' n] `Set.isSubsetOf` s)
+
+cStr :: Char -> Int -> String
+cStr c n = printf "%c%02d" c n
+
+xyStrsUntilN :: Int -> [String]
+xyStrsUntilN n = [printf "%c%02d" xy a | xy <- ['x', 'y'], a <- [0 .. n]]
+
+correctUntilN :: Int -> Map.Map String TreeEntry -> Bool
 correctUntilN n zs = unsafePerformIO $ isTheorem $ do
-  bools <- (fmap (Map.fromList . uncurry zip) . \(a, b) -> (a,) <$> b) $ liftA2 (,) id sBools [printf "%c%02d" xy a | xy <- ['x', 'y'], a <- [0 .. 44 :: Int]]
+  bools <- (fmap (Map.fromList . uncurry zip) . \(a, b) -> (a,) <$> b) $ liftA2 (,) id sBools (xyStrsUntilN 44)
   let zn = zs Map.! (printf "z%02d" n)
   let control = isOdd $ ((asSint bools 'x' + asSint bools 'y') .>>. n) `sMod` 2
-  evalzn <- evalEntry3 bools zn
+  evalzn <- evalTreeEntry bools zn
   return $ evalzn .<=> control
 
 isOdd :: SInt64 -> SBool
 isOdd x = (x `sMod` 2) .== 1
 
--- wd45 = wds Map.! "z45"
--- wd7 = wds Map.! "z07"
--- -- probwrong2 = foldl Set.difference probkpvwrongpds wds
--- wds = Map.map (wd) zs
--- -- probwrongpds = Set.union wd45 wd7
--- wd = wiredeps mappings
--- first = solved Map.! "z00"
--- solved = Map.map (solve3 mappings) zs
--- zs = Map.filterWithKey (\k _ -> head k == 'z') mappings
--- mappings = parseInput2 input
-
--- checkEquivalence :: Entry3 -> Entry3 -> Bool
--- Check equivalence of two Entry3 objects
--- checkEquivalence :: Entry3 -> Entry3 -> Symbolic SBool
--- checkEquivalence e1 e2 = do
---   result1 <- evalEntry3 e1
---   result2 <- evalEntry3 e2
---   return $ result1 .<=> result2
-
--- checkdeps :: String -> (Set.Set String, Entry2) -> Bool
--- -- checkdeps k s | traceShow (k, s) False = undefined
--- checkdeps k (s, e) = expected == s && ()
---   where
---     expected = Set.fromList [printf "%s%02d" xy a :: String | xy <- ["x", "y"], a <- [0 .. numk]]
---     numk = read $ tail k :: Int
-
 solve :: Map.Map String Entry -> Entry -> Entry
-solve m l@(Lit _) = l
-solve m e = case e of
-  Or a b -> Lit ((\(Lit la, Lit lb) -> la .|. lb) $ sol a b)
-  And a b -> Lit ((\(Lit la, Lit lb) -> la .&. lb) $ sol a b)
-  Xor a b -> Lit ((\(Lit la, Lit lb) -> la `xor` lb) $ sol a b)
+solve _ l@(LitRoot _) = l
+solve m (SymWire _ fname a b) = LitRoot ((\(LitRoot la, LitRoot lb) -> f la lb) $ sol a b)
   where
-    sol a b = both (solve m) (m Map.! a, m Map.! b)
+    sol a' b' = both (solve m) (m Map.! a', m Map.! b')
+    f = case fname of
+      '|' -> (.|.)
+      '&' -> (.&.)
+      'x' -> xor
 
-solve3 :: Map.Map String Entry -> Entry -> Entry3
-solve3 m (Orig l) = Rt l
-solve3 m e = case e of
-  Or a b -> sol "||" a b
-  And a b -> sol "&&" a b
-  Xor a b -> sol "xor" a b
-  where
-    sol f a b = uncurry (Fn f) $ both (solve3 m) (m Map.! a, m Map.! b)
+buildtree :: Map.Map String Entry -> Entry -> TreeEntry
+buildtree _ (SymRoot l) = Root l
+buildtree m (SymWire l f a b) = uncurry (Wire l f) $ both (buildtree m) (m Map.! a, m Map.! b)
 
-evalEntry3 :: Bools -> Entry3 -> Symbolic SBool
-evalEntry3 bs (Rt b) = return $ bs Map.! b
-evalEntry3 bs (Fn fname a b) = do
-  a' <- evalEntry3 bs a
-  b' <- evalEntry3 bs b
+evalTreeEntry :: Bools -> TreeEntry -> Symbolic SBool
+evalTreeEntry bs (Root b) = return $ bs Map.! b
+evalTreeEntry bs (Wire _ fname a b) = do
+  a' <- evalTreeEntry bs a
+  b' <- evalTreeEntry bs b
   return $ f a' b'
   where
     f = case fname of
-      "||" -> (.||)
-      "&&" -> (.&&)
-      "xor" -> (.<+>)
+      '|' -> (.||)
+      '&' -> (.&&)
+      'x' -> (.<+>)
 
-deps :: Entry3 -> Set.Set String
-deps (Rt s) = Set.singleton s
-deps (Fn _ a b) = Set.union (deps a) (deps b)
+deps :: TreeEntry -> Set.Set String
+deps (Root s) = Set.singleton s
+deps (Wire _ _ a b) = Set.union (deps a) (deps b)
 
-wiredeps :: EMap -> Entry -> Set.Set String
-wiredeps m (Orig l) = Set.empty
-wiredeps m e = case e of
-  Or a b -> Set.unions (Set.fromList (f a b) : sol a b)
-  And a b -> Set.unions (Set.fromList (f a b) : sol a b)
-  Xor a b -> Set.unions (Set.fromList (f a b) : sol a b)
+wiredeps :: TreeEntry -> Set.Set String
+wiredeps (Root _) = Set.empty
+wiredeps (Wire l _ a b) = Set.unions [Set.singleton l, wiredeps a, wiredeps b]
+
+data TreeEntry = Root String | Wire String Char TreeEntry TreeEntry
+
+instance Show TreeEntry where
+  show :: TreeEntry -> String
+  show (Root s) = s
+  show (Wire l f a b) = printf "(%s: %s %c %s)" l (show a) f (show b)
+
+data Entry = LitRoot Int | SymRoot String | SymWire String Char String String
+  deriving (Show, Eq, Ord)
+
+parseInput :: Part -> [String] -> EntryMap
+parseInput p input = foldl (\m (r, g) -> Map.insert r g m) withtop bottom'
   where
-    f a b = filter ((`notElem` ['x', 'y']) . head) [a, b]
-    sol a b = map (wiredeps m) [m Map.! a, m Map.! b]
-
-data Entry3 = Rt String | Fn String Entry3 Entry3
-
-instance Show Entry3 where
-  show (Rt s) = s
-  show (Fn f a b) = "(" ++ show a ++ " " ++ f ++ " " ++ show b ++ ")"
-
-data Entry = Lit Int | Or String String | And String String | Xor String String | Orig String
-  deriving (Show, Eq)
-
-parseInput2 :: [String] -> Map.Map String Entry
-parseInput2 input = foldl (\m (r, g) -> Map.insert r g m) withtop bottom'
-  where
-    bottom' = map ((\[g, r] -> (r,) . parsegate $ splitOn " " g) . splitOn " -> ") bottom
-    parsegate [a, "AND", b] = And a b
-    parsegate [a, "OR", b] = Or a b
-    parsegate [a, "XOR", b] = Xor a b
-    withtop = foldl (\m [a, _] -> Map.insert a (Orig a) m) Map.empty $ map (splitOn ": ") top
-    [top, bottom] = splitOn [""] input
-
-parseInput :: [String] -> EMap
-parseInput input = foldl (\m (r, g) -> Map.insert r g m) withtop bottom'
-  where
-    bottom' = map ((\[g, r] -> (r,) . parsegate $ splitOn " " g) . splitOn " -> ") bottom
-    parsegate [a, "AND", b] = And a b
-    parsegate [a, "OR", b] = Or a b
-    parsegate [a, "XOR", b] = Xor a b
-    withtop = foldl (\m [a, b] -> Map.insert a (Lit (read b)) m) Map.empty $ map (splitOn ": ") top
+    bottom' = map ((\[g, l] -> (l,) . parsegate l $ splitOn " " g) . splitOn " -> ") bottom
+    parsegate l [a, gate, b] = SymWire l (tochar gate) a b
+    tochar gate = case gate of
+      "AND" -> '&'
+      "OR" -> '|'
+      "XOR" -> 'x'
+    insertRoot m [a, b] = case p of
+      Part1 -> Map.insert a (LitRoot (read b)) m
+      Part2 -> Map.insert a (SymRoot a) m
+    withtop = foldl insertRoot Map.empty $ map (splitOn ": ") top
     [top, bottom] = splitOn [""] input
