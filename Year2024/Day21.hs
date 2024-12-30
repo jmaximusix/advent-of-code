229c4e53c3c936bf183fd21538eeecbfb8a18639
JMaximusIX <nix@gibts>
2024-12-30 13:57:14 +0100

    O (n log n) matrix approach

--- Year2024/Day21.hs
+++ Year2024/Day21.hs
@@ -1,75 +1,93 @@
+{-# LANGUAGE DataKinds #-}
+
 module Day21 (part1, part2) where
 
-import Data.Bifunctor (bimap, first)
+import Data.Bifunctor (first)
 import qualified Data.Map.Strict as Map
-import Linear (V2 (V2))
+import Data.Maybe (fromJust)
+import GHC.IsList (fromList)
+import Linear (V2 (V2), dot, (!*))
+import Linear.V (V, fromVector)
+import MyLib.Utils (matrixPower)
 
--- since my solution runs in linear time I'm using Integers just for fun and can calculate up to 10k robots in <1s
+type V13 = V 13
+
+type Matrix13 a = V13 (V13 a)
+
+toV13 :: [a] -> V13 a
+toV13 = fromJust . fromVector . fromList
+
+-- since this solution runs in O(n log n) I'm using Integers just for fun
 part1, part2 :: [String] -> Integer
 part1 = solve 2
 part2 = solve 25
 
 parseInput :: String -> (String, Integer)
-parseInput s = ('A' : s, read $ init s)
+parseInput s = (s, read $ init s)
 
 solve :: Int -> [String] -> Integer
-solve n = sum . map (uncurry (*) . first (fst . chainNRobots n . map (numpad Map.!)) . parseInput)
+solve n = sum . map (uncurry (*) . first (chainNRobots n) . parseInput)
+
+chainNRobots :: Int -> [Char] -> Integer
+chainNRobots n = fst . foldl (\(count, l) l' -> (count + moves l l', l')) (0, 'A')
   where
-    numpad =
-      Map.fromList
-        [ ('7', V2 0 0),
-          ('8', V2 1 0),
-          ('9', V2 2 0),
-          ('4', V2 0 1),
-          ('5', V2 1 1),
-          ('6', V2 2 1),
-          ('1', V2 0 2),
-          ('2', V2 1 2),
-          ('3', V2 2 2),
-          ('0', V2 1 3),
-          ('A', V2 2 3)
-        ]
+    transitions = matrixPower arrowmatrix n
+    moves l l' = (transitions !* (keypad l l')) `dot` (toV13 (1 : replicate 12 0))
 
-chainNRobots :: Int -> [V2 Integer] -> (Integer, Map.Map [Char] Integer)
-chainNRobots 0 [a, b] = bimap (+ 1) (`Map.singleton` 1) (keypad a b)
-chainNRobots 0 (a : b : r) = bimap sum (Map.unionsWith (+)) $ unzip [chainNRobots 0 [a, b], chainNRobots 0 (b : r)]
-chainNRobots n keysToPress = Map.foldlWithKey update (total, Map.empty) moves
-  where
-    (total, moves) = chainNRobots (n - 1) keysToPress
-    update (acc, movemap) move c =
-      let (dacc, newmoves) = arrowpad move
-       in (acc + c * dacc, foldl (\mm newm -> Map.insertWith (+) newm c mm) movemap newmoves)
+keymap :: Map.Map Char (V2 Int)
+keymap =
+  Map.fromList
+    [ ('1', V2 0 0),
+      ('2', V2 1 0),
+      ('3', V2 2 0),
+      ('4', V2 0 1),
+      ('5', V2 1 1),
+      ('6', V2 2 1),
+      ('7', V2 0 2),
+      ('8', V2 1 2),
+      ('9', V2 2 2),
+      ('A', V2 1 3),
+      ('0', V2 0 3)
+    ]
 
-arrowpad :: String -> (Integer, [String])
-arrowpad "v" = (4, ["<v", "^>"])
-arrowpad "^" = (2, ["<", ">"])
-arrowpad ">" = (2, ["v", "^"])
-arrowpad "<" = (6, ["v<", ">^"])
-arrowpad "v>" = (4, ["<v", ">", "^"])
-arrowpad ">v" = (4, ["v", "<", "^>"])
-arrowpad ">^" = (4, ["v", "<^", ">"])
-arrowpad "^>" = (4, ["<", "v>", "^"])
-arrowpad "<v" = (6, ["v<", ">", "^>"])
-arrowpad "<^" = (6, ["v<", ">^", ">"])
-arrowpad "^<" = (6, ["<", "v<", ">^"])
-arrowpad "v<" = (6, ["<v", "<", ">^"])
-
-keypad :: V2 Integer -> V2 Integer -> (Integer, String)
-keypad v1@(V2 x1 y1) v2@(V2 x2 y2)
-  | x2 == 0 && y1 == 3 = (d, "^<")
-  | x1 == 0 && y2 == 3 = (d, ">v")
+keypad :: Char -> Char -> V13 Integer
+keypad c1 c2
+  | x1 == 0 && y2 == 3 = keyV 10
+  | x2 == 0 && y1 == 3 = keyV 12
   | otherwise = case sv of
-      V2 0 1 -> (dy, "v")
-      V2 0 (-1) -> (dy, "^")
-      V2 1 0 -> (dx, ">")
-      V2 (-1) 0 -> (dx, "<")
-      V2 1 1 -> (d, "v>")
-      V2 1 (-1) -> (d, "^>")
-      V2 (-1) 1 -> (d, "<v")
-      V2 (-1) (-1) -> (d, "<^")
+      V2 0 (-1) -> keyV 1
+      V2 1 0 -> keyV 2
+      V2 0 1 -> keyV 3
+      V2 (-1) 0 -> keyV 4
+      V2 1 (-1) -> keyV 5
+      V2 1 1 -> keyV 6
+      V2 (-1) 1 -> keyV 7
+      V2 (-1) (-1) -> keyV 8
   where
+    v1@(V2 x1 y1) = keymap Map.! c1
+    v2@(V2 x2 y2) = keymap Map.! c2
     (V2 x y) = v2 - v1
     sv = V2 (signum x) (signum y)
-    dx = abs x
-    dy = abs y
-    d = dx + dy
+    d = abs x + abs y
+    keyV i =
+      toV13 $
+        (fromIntegral d + 1 : replicate (i - 1) 0)
+          ++ (1 : replicate (12 - i) 0)
+
+arrowmatrix :: Matrix13 Integer
+arrowmatrix =
+  toV13 . map toV13 $
+    [ [1, 2, 2, 4, 6, 4, 4, 6, 6, 4, 4, 6, 6],
+      [0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+      [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1],
+      [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
